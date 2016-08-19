9a279743b296345141581f8f61992bc916d44bdc
Radoslaw Oska <radoslaw.oska@me.com>
2016-08-19 08:01:02 +0200

    new entries added

---
+++ 6820.pl
@@ -0,0 +1,67 @@
+Potęga Miłości, (fragment z wykładu "Talk To Sahaja Yogis, Open Your Heart, Seminar 4th Session, Lonavala (India), 25.01.1982)
+
+Sahaja Yoga jest miejscem, gdzie musicie się uczyć, w każdy możliwy sposób. Hindusi muszą się uczyć, jak być uczciwym a Anglicy muszą się uczyć jak kochać. Amerykanie muszą się uczyć jak szanować samych siebie. A my wszyscy musimy się nauczyć, jak porzucić swoje wcześniejsze identyfikacje i musimy stać się subtelniejsi i uniwersalni i ludźmi tak wartościowymi, że każdy, kto ich zobaczy, powie,  że Matka stworzyła w Ameryce Abrahama Lincolna, w Rosji Tołstoja, a w Maharastrze Shivajiego.
+
+Jestem zbyt ambitna, powinnam powiedzieć, ale widzę rzeczywistość w was i rzeczywistość w Samej Sobie. Na tej podstawie mówię, że trzeba stworzyć ludzi tej wyjątkowej jakości, którzy mają znakomite cechy, wysokiej próby cechy, duchowe cechy, które są tak subtelne, tak piękne, tak słodcy, tak wyrozumiali. A nadal jak stal, nikt nie może odwieźć ich od ich własnych zasad, tatw.  To jest to, takie proste.  A wtedy zaczniecie radować się swoją miłością, swoimi zaletami, cieszyć się wszystkim i mieć współczucie dla innych.
+
+Wtedy nie łapiecie blokad; łapiecie, ponieważ nie macie współczucia. Mając współczucie, nigdy nie złapiecie, nigdy. Matka nigdy nie zarazi się chorobą od dziecka. Ona nigdy nie zarazi się chorobą od swojego dziecka, to jest fakt. Dziecko może być chore na gruźlicę lub na cokolwiek innego, ona nigdy nie złapie, ponieważ ona kocha. W ten sam sposób, kiedy naprawdę kochacie, nigdy niczego nie złapiecie. Po co kwestionować coś oczywistego, gdzie się oczyszczać, co robić - jakie to ma znaczenie?  Po prostu płyniecie swoim sercem.
+
+W tym momencie, moje dzieci, moi Sahaja yogini  są na takim poziomie, że całkowicie otwierają swoje serce,  bez żadnego lęku. To jest zupełnie inny poziom, który osiągnęliście,  teraz jesteście już na innym poziomie. Tylko przyjmijcie swoje moce. Wiedzcie, że jesteście tacy. Wystarczy otworzyć swoje serce. Pewnego dnia spotkałam kogoś, nałapał okropnych blokad ze wszystkich stron, on nie był yoginem, był kimś innym.
+
+Zapytałam go:
+
+“Jak udało ci się nałapać w siebie tak wiele buców?”
+
+Każdy, każdy guru jakiego wymieniłeś, był tam.
+
+Zapytałam: “Jak zdołałeś to zrobić?”
+
+A on odpowiedział: “Ja mam bardzo otwarte serce”.
+
+“O, Boże” powiedziałam.
+
+Odrzekłam: “To jest jak otwarte okno w zanieczyszczonym miejscu, wleci każdy odór, każdy komar i wpełzną robaki“.
+
+Zatem lęk przed otwarciem serca do takiego stopnia,  przychodzi stąd, z takiego doświadczenia. Ale teraz jesteście na takim poziomie, otwórzcie swoje serce. Nie powinniście się bać; nie będzie problemu,  materialne problemy nie są trudne do rozwiązania.
+
+Kiedyś ktoś zabrał ze sobą moją fotografię,  która była w srebrnej ramce, i martwił się, że celnik może ją zatrzymać .
+
+Oczywiście fotografii oni nie mogą zatrzymać,  ale być może srebrną ramkę mogą. I ten pan powiedział Mi, że on brał ją  ze sobą w tej ramce, srebrnej ramce i myślał, że ludzie będą mieli coś przeciwko temu. Ale jak tylko zobaczyli fotografię, powiedzieli ‘namaskar’ do fotografii i zapomnieli o ramce.
+
+Tak wiele problemów może zostać rozwiązanych, materialnych problemów - to jest takie proste do rozwiązania, takie proste. Najłatwiej jest je rozwiązać, problemy fizyczne również. Znałam paru Sahaja yoginów,  którzy byli w Sahaja yodze sześć, siedem lat. Ale nadal boją się o swoje ciało, fizyczne ciało, mieli kłopoty takie i inne… Jeśli uda im się porzucić ten strach, oni wyjdą z tego.
+
+To jest takie błędne koło wbudowane w nich. I ta sama rzecz wypracuje się poprzez operację  na otwartym sercu, jak my to nazywamy. Obawiamy się, że nasze serce zostanie ponownie  złapane w bezsensowne rzeczy, więc nie chcemy otworzyć naszego serca. Ten strach mogę zrozumieć.
+
+Ale teraz jesteście Sahaja yoginami, skończone. Powiedzcie tym wszystkim ludziom, żeby się ulotnili. Wszystkim tym lękom, powinniście powiedzieć, aby przepadły. Kiedy odkryjecie, że jesteście Duchem,  jesteście takim potężnym Duchem, po prostu starajcie się ustabilizować. Jakież to daje poczucie szczęścia. To takie proste. Kiedy naprawdę czujecie się szczęśliwi? Wtedy, kiedy coś dajecie, nie tylko dla Shri Mataji. Dla Mnie jesteście, Ja wiem. Jeśli wezmę coś od was, będziecie się czuli bardzo szczęśliwi z tego powodu. Ja wiem. Ale dla kogoś jeszcze; dajcie trochę miłości, komfortu.  Jak wielką moc ma głos i taka mowa, że możecie sprawiać innym przyjemność przez mówienie dobrych rzeczy.
+
+Czy możecie sobie wyobrazić, jaką moc dał wam Bóg? Bez wydawania choćby grosza, możecie sprawiać innym przyjemność,  tylko mówiąc dobre rzeczy. Jeśli powiecie mantrę, sprawicie im większą przyjemność w subtelny sposób.
+
+Jeśli zaszczepicie w nich miłość do Boga, to jest jeszcze wspanialsze.  A radość z tego jest tak wielka, ponieważ Duch jest radością. Im bardziej go otworzycie na zewnątrz, tym więcej światła wejdzie do środka, to więcej wypracuje. Zatem porzućcie ten strach, który jest osadzony w waszych doświadczeniach.
+
+Teraz to jest nowe doświadczenie nowego życia, gdzie wasza energia przychodzi od Ducha, który jest miłością.
+
+W jednym słowie to jest miłość.
+
+A w trzech słowach, to jest prawda, to jest uwaga i to jest radość.
+
+Wszystko złożone razem - to jest miłość.
+
+Kiedy je połączycie, staną się miłością.
+
+I to jest to, co macie - to wielka siła miłości.
+
+Poprzez swoją uwagę kochacie innych,  nawet uwaga jest wystarczająca. Aby być uprzejmym .... Widzisz, Ja potrzebowałam chusteczkę. Dobrze, to prawda. Potrzebowałam chusteczkę. Zapomniałam jej. Mam na myśli, że przeważnie zapominam tych wszystkich rzeczy. Wtedy oni czują, że coś jest potrzebne: “Matka po prostu potrzebuje chusteczki”. Oni przynoszą chusteczkę. Bardzo prosta akcja, wiecie, ale ich uwaga jest na Mnie.  Tak drobnymi rzeczami jak chusteczka można sprawić innym przyjemność. Dla Sahaja yogina nawet danie samej chusteczki,  daje takie wibracje.  Aby zdobywać innych ludzi, jest też taki sposób jak bycie dla nich miłym i hojnym.  Ale danie im Realizacji jest czymś najwyższym.
+
+Teraz, kiedy stajecie się coraz subtelniejsi,  będziecie widzieć, że nie darzycie sympatią ludzi, chorych i strapionych, nie ma sympatii, ale będziecie czuć dla nich miłość. Jest duża różnica pomiędzy sympatią a miłością.  Co oznacza posiadanie sympatii? Uczestniczenie w patosie. Patos. Jeśli chcecie uczestniczyć w patosie, to uczestniczcie. To bardzo dobry pomysł uczestniczyć w patosie. Ale jeśli chcecie uczestniczyć w radości,  nie będziecie mieli sympatii, będziecie mieli miłość. Chcecie dzielić się radością swojego Ducha z inną osobą, więc możecie mówić do tej osoby w sposób,  który być może zbytnio jej nie zadowoli ani nie padnie jej do stóp,  i widzicie, to jest robota polityków albo biznesmenów, nie wasza. Nie chcecie robić na nich biznesu, chcecie doprowadzić ich do Realizacji. 
+
+Więc musicie mówić niewygodne rzeczy bardzo stanowczo. Z całą mocą musicie mówić: “To jest absolutnie złe, absurdalne”. Musicie ich przekonać w taki sposób,  żeby nie powtarzać, że to jest tak i tak, to nigdy nie dotrze do ich głów, bo są bucami i musicie porządnie nimi potrząsnąć. To też jest miłość.  Zatem miłość jest czymś najmocniejszym i najsłodszym, tak pięknie połączonym,  i tylko ten, kto ma mądrość, tą subtelną mądrość, może to osiągnąć.
+
+Teraz umysł znowu będzie pracował:
+
+“Jak to zrobić?”
+
+Myślę, że wszyscy powinniście to słowo “jak” wyrzucić do morza.  Jest inne bardzo dobre słowo w języku angielskim,  które powinniście używać: “Dlaczego nie!” W ten sposób możecie się upewnić i uspokoić, wypracujcie to w ten sposób do całkowitego błogosławieństwa Bożego.  Musicie tylko przekazać te błogosławieństwa innym,  to wszystko. Nie ma nic więcej do zrobienia, tylko przekazać to światło innym.To, czego wam do tego trzeba - to człowiek, który akceptuje  miłość jako swoją karierę życiową.
+
+Człowieka, który wielbi miłość.
+
+Osobę, która uważa, że miłość jest ponad wszystkim.
