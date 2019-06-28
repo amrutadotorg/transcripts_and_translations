939e05bbc65f90917c836606d0110b49b2d5239e
amrutadotorg <amruta.org@gmail.com>
2019-06-28 23:00:04 +0200

    new entries added

---
+++ 19723.pl
@@ -0,0 +1,131 @@
+Wywiad w Telewizji, 23.09.1983, Los Angeles, USA
+
+Właśnie dołącza do mnie Shri Mataji, znana na całym świecie jako święta. 
+Po raz pierwszy udało mi się spotkać świętą. I jesteś też uzdrowicielką poprzez wiarę albo uzdrowicielką poprzez psychikę. Przypuszczam, że niektórzy ludzie tak ciebie określają. Ale zapytana niedawno w wywiadzie, czy jesteś świętą albo czy uważasz się za świętą, udzieliłaś niezwykłej odpowiedzi. Jesteś więc? 
+
+Właściwie nie możesz sama sobie dać certyfikatu, czy czegoś takiego? Jakość świętego polega na tym, że może on tworzyć innych świętych. Jeśli potrafi, innych uczynić świętymi, wtedy jest świętym. W przeciwnym razie jest to po prostu samoocena.
+
+Czy potrafisz czynić innych świętymi?
+
+Myślę, że tak.
+
+Jak zdefiniowałbyś centrum? Czym stałaby się inna osoba, która uczyniłaby go świętym?
+
+Osoba, która staje się świętą, rozwija nową świadomość w wyniku żywego procesu ewolucyjnego, ona staje się Duchem, tak jak to opisuje Chrystus: "Masz narodzić się ponownie". I w tym, świadomość staje się jej urzeczywistnieniem. To nie tylko mentalna projekcja, ale urzeczywistnienie, że stajesz się osobą, która jest kolektywnie świadoma.
+
+Wspomniałaś o Chrystusie, a przecież pochodzisz ze wschodniego kraju z Indii. Czy wierzysz w Jezusa Chrystusa?
+
+Oczywiście, On odgrywa bardzo ważną rolę w waszej ewolucji. On jest bramą, jak powiedziano.
+
+Zatem według waszej definicji, święty może być osobą, która może być narodzonym na nowo chrześcijaninem, który mógłby przyjąć zbiorową świadomość Jezusa do swojego życia?
+
+Widzisz, ci którzy przyjmują go mentalnie, nie rodzą się ponownie. Myśl, to jest wydarzenie, które musi zaistnieć. Chrzest to wydarzenie, które dzieje się, gdy siła życiowa istniejąca w naszej kości krzyżowej unosi się. Możecie poczuć unoszenie się tej siły, która dochodzi do ciemiączka - [wtedy] będziemy ochrzczeni - i poczujecie chłodny powiew Ducha Świętego.
+
+Tak, oni mówią, że jest chłodny powiew, powiew Ducha Świętego. To ludzie czują.
+
+Tak.
+
+I kolejne wywiady, w których przeczytałam, mówiłaś, że umieściłaś dłoń nad głową tego, który przeprowadzał z Tobą wywiad.
+
+Tak.
+
+I on był w stanie poczuć chłodny powiew.
+
+Tak, tak. A także z....
+
+Czy możesz dać mi ten chłodny powiew? Przez całe moje życie czekałam na to.
+
+Tak, myślę, że uzyskałaś to.
+
+Tak, rzeczywiście? Na początku byłaś trochę podenerwowana....
+
+Byłam zdenerwowana dziś rano.
+
+I wszystko jest w porządku.
+
+Tak.
+
+Ale teraz powinnaś się uspokoić i wszystko się ułoży w mgnieniu oka.
+
+Czy ja też będę miała chłodny powiew?
+
+Oczywiście.
+
+Czy musisz mnie dotknąć?
+
+Nie, nie, nie, nie. To może pomóc, rozmawiając ze mną, możesz to dostać, ale w rzeczywistości poczujesz to na koniuszkach palców. Jak powiedział Mahomet: "W czasie zmartwychwstania wasze ręce przemówią". I po raz pierwszy czujesz tę Wszechprzenikającą subtelną moc, że stajesz się czymś więcej niż jesteś. Tym właśnie jest święty.
+
+Uzdrawiałaś wielu ludzi, jak myślisz ilu z nich zostało uzdrowionych w ciągu twojego życia?
+
+Nie wiem ilu, nigdy nie liczyłam.
+
+Setki?
+
+Tak, co najmniej. Przynajmniej, co najmniej setki. Tak, co najmniej setki. Ale tak naprawdę ludzie, którzy mogą zostać świętymi, również mogą [uzdrawiać]. Więc jeśli widzisz Sahaja Yogę, mogą to być tysiące. Ponieważ pojawiło się tak wielu ludzi, którzy stali się prorokami. Oni uzdrawiają także innych ludzi. Tak, oni też. Oni uzdrawiają znacznie więcej niż obecnie ja sama.
+
+Jakiego rodzaju choroby lub sprawy udało ci się wyleczyć?
+
+Większość nieuleczalnych chorób zostaje uleczona, ale to nie jest uzdrawianie przez wiarę, w tym sensie, że nie musicie wierzyć, albo nie musicie wierzyć w cokolwiek. Nie ma w tym zaślepienia. Nie ma zaślepienia. Właściwie to dzieje się tak, jak kiełkowanie nasionka. Tak jak nasionko kiełkuje, w ten sam sposób ma miejsce wasze przebudzenie. A dzięki temu, odżywiony jest przywspółczulny układ nerwowy. To kwestia odżywienia. Odżywienie, tak to działa.
+
+Wielokrotnie wypowiadałaś się na temat religii prawosławnej, katolicyzmu i protestantyzmu.
+
+O każdym "-iźmie".
+
+Co o nich myślisz? Jak je klasyfikujesz, czym się wyróżniają?
+
+Wszystkie religie, jak widzicie, są niczym innym jak filozofiami. Są jedynie filozofiami. Są to słowa, słowa i słowa. Sam Chrystus - teraz zobaczmy, On powiedział: "Nie będziesz miał cudzołożnych oczu". Czy możesz znaleźć jakiegokolwiek chrześcijanina bez cudzołożnych oczu, to raczej trudne.
+
+Hm, hm.
+
+Czemu? Dlaczego tak jest, kiedy On to powiedział, dlaczego tak jest, że nie możemy mieć tych niewinnych oczu? Powinniśmy mieć! Powodem jest to, co powiedział: "On ma narodzić się ponownie w tobie". I jest pewne miejsce, w którym Chrystus budzi się, kiedy ta siła się podnosi.
+
+I tutaj, w jednym z artykułów, które przeczytałam, powiedziano, że czułaś, iż niektóre religie prawosławne były oparte na ignorancji i ekstremizmie. Więc zabrzmiało to dla mnie 
+jak fanatyzm, 
+że byłaś wobec nich negatywnie [nastawiona]. 
+
+Nie, nie bardzo negatywnie. Powinnam powiedzieć, jako matka, że ​​zostali sprzeniewierzeni, to oni wprowadzają ludzi w błąd. 
+Wprowadzają w błąd. 
+Widzicie, oni po prostu zaczęli myśleć, że czytając coś lub modląc się, to się wypracuje. Musicie być najpierw połączeni z Bogiem. 
+
+Jak uzyskać połączenie?
+
+Połączenie - Bóg umieścił wszystko w was. Wszystko jest w was i może to zrobić tylko osoba, która jest świętą. To jest jak Matka Ziemia: wkładacie do niej nasionko i ono samo wykiełkuje. To jest wasze własne.
+
+Hm, hm.
+
+Więc jest spontaniczne, to jest żywy [proces].
+
+Jest taki artykuł w londyńskiej gazecie, który wspomniał, że młody 17-letni chłopiec został ciężko ranny prawie do tego stopnia, że ​​został zmasakrowany na motocyklu. I był on - powiedzmy - dwie mile od miejsca, w którym Ty byłaś. A jednak on twierdzi, że widział, jak do niego przychodzisz. Myślał, że naprawdę tam jesteś, widocznie to była wizja, że ​​położyłaś dłonie na różnych częściach jego ciała i został uzdrowiony. ...Ten przypadek zobaczenia Ciebie tam, ale przecież Ty tam byłaś. W jaki sposób?
+
+To prawda, to zadziałało.
+
+Jak możesz to wyjaśnić?
+
+Widzicie, musimy wiedzieć, że w Królestwie Bożym cała wasza telekomunikacyjna telewizja jest tak skuteczna, to działa, widzicie. Tak jak tutaj siedzę, ale jestem tam i jestem wszędzie, jak wtedy mogliście to zobaczyć. W ten sam sposób w jego "telewizji" mają takie aranżacje i możecie to wypracować. To jest proste.
+
+Powiedziałaś też, że jesteś w stanie wyleczyć AIDS. W jaki sposób?
+
+Tak, można to wyleczyć, ponieważ jest to - co widziałam poprzez tę Zbiorową Świadomość, że AIDS jest spowodowane kombinacją trzech subtelnych ośrodków w nas. A jeśli te trzy ośrodki mogą zostać odżywione, można je wyleczyć. Myślę, że jeden człowiek w Londynie został wyleczony.
+
+Ale w innym wywiadzie powiedziałaś także coś takiego, że jeśli wy, jeśli wszyscy staniecie się kolektywnie świadomi lub jednością z Tobą albo tak samo świadomi jak chcesz Ty to przedstawić, że nie będzie już więcej homoseksualizmu. Więc czy twierdzisz, że homoseksualizm jest czymś negatywnym?
+
+Nie, to jest nienaturalne.
+
+Nienaturalne?
+
+To nie jest naturalne. Chodzi o to, że jeśli oni stają się homoseksualni, to jest z powodu przymusowych warunków. Na przykład, powiedzmy, widzicie, jeśli oni widzą, że kobiety są zbyt dominujące, trudno jest z nimi dalej żyć, one nie są już kobietami, one są bardziej mężczyznami, widzisz, oni wolą mężczyzn, którzy są kobiecy, to mieszanka. Społeczeństwo jest dość pomieszane. Możliwe też, że czasami mężczyzna może być opętany przez kobietę i ona-on może zachowywać się w ten sposób. To skomplikowana osobowość.
+
+Jerry Falwell był w jej programie w zeszłym tygodniu i wydaje mi się, że też tydzień wcześniej, on powiedział, że stanem naturalnym, który wyleczyłby wiele rzeczy, jest monogamia: jeden mężczyzna dla jednej kobiety. Czy się z tym zgadzasz?
+
+Oczywiście, uważam, że to jest naturalne, że tak jest! Bez problemów.
+
+On powiedział, że nasze społeczeństwo, społeczeństwo stało się zbyt rozwiązłe i dlatego mamy te wszystkie nowe choroby.
+
+Tak. Widzisz, chodzi o społeczeństwo zorientowane na ego i gdzie myślimy: "A co w tym złego?" Ale nie widzimy całości, widzimy tylko część tego. A ego nigdy nie doprowadzi nas do właściwych rzeczy. Nasza Jaźń jest tą, która daje nam całe bezpieczeństwo, całą mądrość, wszystkie wskazówki. A potem automatycznie stajemy się monogamiczni. Nie muszę tego mówić. Większość ludzi, którzy zostali prorokami, jak powiedziałam, wyleczyli się i stali się monogamiczni. Niektórzy z nich byli homoseksualistami, stali się normalnymi ludźmi, ożenili się.
+
+W porządku. Przekroczyliśmy już czas. 
+Chciałbym dłużej z Tobą porozmawiać, niektóre z Twoich pomysłów są bardzo fascynujące. Dla tych z was, którzy chcieliby pójść na darmowy wykład, nie wiem, czy możemy to nazwać "wykład", ale doświadczenie ze Shri Mataji, są trzy spotkania, które będą dla was dostępne - będziecie mogli spotkać ją tutaj w Los Angeles. Najpierw w sobotę w Pierwszym Kościele Metodystów przy Franklin Avenue właśnie tutaj w Hollywood. A także ponownie w poniedziałek i we wtorek o 7 rano, więc w tę sobotę w First United Methodist Church of Hollywood. 
+
+Bardzo dziękuję.
+
+Dziękuję.
---
+++ 2805.pl
@@ -0,0 +1,38 @@
+Mahashivaratri Puja, Guru Pad, 15.02.2004, Pune, Indie (Tłumaczenie z Hindi)
+
+Dzisiaj jesteśmy tutaj, aby celebrować puję Guru.
+
+Guru jest bardziej sławiony niż Bóstwa i Bogowie. Kim jest ten Guru? W tym, co jest najważniejszą shakti? Zasada Guru jest Shivą. Tę formę mocy Shivy powinniśmy uważać za moc Guru. Ponieważ, kiedy przyjmujemy moce od shakti Guru, i kiedy shakti wchodzą w nas, wtedy sami stajemy się Guru. I dzięki temu jesteśmy błogosławieni przez całe życie - Kalyan. Ten, kto dostaje te moce, wydaje się, że otrzymuje wszystkie te błogosławieństwa kalyan. "Kalyan" to wyraz, który jest bardzo trudny do wyjaśnienia w kilku słowach.
+
+"Kalyan" oznacza otrzymanie błogosławieństwa ze wszystkich stron, uzyskanie radości ze wszystkich stron.
+
+Kiedy ktoś mówi, że w tych błogosławieństwach otrzymujecie Kalyana, to co to znaczy? Co to jest Kalyan? Kalyan oznacza Atma Sakshatkar, Samorealizację. Bez Samorealizacji nie możecie mieć Kalyana. Nikt nie może zrozumieć bez posiadania tego. Wszystkie te rzeczy są w Kalyan. Z tego powodu człowiek otrzymuje całą radość i staje się rozpromieniony. Macie Kalyan, cokolwiek musicie zrobić, już to wszystko zrobiliście, jakiekolwiek wysiłki musicie podjąć, już je podjęliście, wiara, którą musicie przyjąć, już ją przyjęliście. Ale kiedy wszyscy jesteście w "Kalyanmarg" [ścieżka Kalyan], kiedy on dał mantrę, aby mieć Kalyan, co to znaczy? Oznacza to, że najważniejszą rzeczą w was jest samadhan [zadowolenie].
+
+Potem wy wszyscy nie musicie niczego poszukiwać, wszyscy staliście się Guru. Nie musicie w tym momencie robić wielkich rzeczy, ale błogosławieństwa tej samadhany, wszyscy możecie poczuć wewnątrz siebie, wszyscy możecie rozpoznać w sobie, wszyscy możecie cieszyć się wewnątrz siebie.
+
+Przede wszystkim wszyscy widzicie, że choroby fizyczne, fizyczne problemy mogą zostać usunięte przez Kalyanmarg, wszystkie problemy, które są w was, które uniemożliwiają rozkwit, mogą zostać usunięte przez ten Kalyanmarg. Kiedy wszyscy to przyjmiecie, otrzymacie Kalyan i macie Samorealizację. Shri Mahadeva wam w tym pomaga. Kiedy Kundalini otwiera Sahastrarę, Shri Mahadeva tam zasiada. I dlatego nazywamy Go Mahadevą. To znaczy Bóg wszystkich Bogów, Mahadeva.
+
+W tym Kalyana jest wiele ważnych aspektów. Pierwszą rzeczą jest shanti [spokój].
+
+Spokój psychiczny, spokój fizyczny i spokój w życiu rodzinnym. W życiu rodzinnym jest wiele problemów, wiele przeszkód, i wszystko to może być rozwiązane przez Kalyanmarg. Potem nie możemy nawet poczuć obecności przeszkód. W ten sposób moglibyśmy zobaczyć wielu ludzi na całym świecie otrzymujących błogosławieństwa z tego Kalyanmarg i medytujących nad nim, i radujących się przez całe życie. 
+To jest Kalyan, przez który ludzkość może uzyskać równowagę w sobie. I aby uzyskać tę równowagę, musicie przejść pod ochronę Guru. Kiedy stajecie się wielbicielem Guru, otrzymujecie równowagę i dostajecie wszystko. Po tym nie potrzebujecie już niczego. W ten sposób uzyskujemy równowagę i uzyskujemy moc miłości wewnątrz siebie. Kiedy ludzkość to otrzymuje, całe to ciało się raduje. 
+
+Kiedy uzyskujecie ten stan, shakti [moce] niszczą wszystkie problemy, zakłócenia w ciele i w stanie fizycznym, i tak wielu ludzi bardzo się stara, aby otrzymać te shakti. On może otrzymać shakti tylko od osoby, która ma Samorealizację. A następnie on sam staje się w formie Boga. Jest to temat, który można wyjaśnić tylko w języku Hindi, który mówi, że tę Guru Padh dostajecie od kogoś innego. Ale ten ktoś inny jest sam w sobie, jest obdarzony mocą, mocą spokoju umysłu, na początek, a także mocą do pokonania wszystkich rodzajów ziemskich problemów, problemów psychicznych, problemów fizycznych.
+
+Wszystkie te problemy można rozwiązać poprzez równowagę psychiczną i mentalne błogosławieństwa od waszego Guru. Kiedy stajecie się Guru, wy sami macie moc, aby błogosławić innych. Poprzez moc waszego błogosławieństwa możecie stworzyć wielu guru. A kiedy Guru zostanie stworzony i pojawi się Guru, który ma tę moc, jest to bardzo satysfakcjonujące i bardzo [niesłyszalne].
+
+Satysfakcja jest tak duża, że ​​niczego nie chcecie.
+
+To jest moc Shivy. Widzieliście Shivę, nie nosi wielu ubrań, Nie nosi wielu ozdób, On po prostu siedzi w medytacyjnym nastroju, przez cały czas. On niczego nie potrzebuje. Jest tak usatysfakcjonowany sam sobą, że niczego nie potrzebuje. I to jest moc, którą otrzymujecie po Samorealizacji, jeśli macie Guru, i to Guru tego poziomu i kalibru.
+
+Nie należy starać się zostać Guru. To bardzo niepraktyczne. Jeśli próbujecie stać się, to nigdy nie będziecie. To musi przyjść do was automatycznie, bez proszenia, bez żadnego wysiłku. Więc jedynym sposobem, aby do tego dojść, jest Dhyana.
+
+Dhyana jest medytacją. Kiedy medytujecie, po prostu medytujcie i medytujcie, nie proście o nic. Medytacja sama daje wam ten instrument, który może znieść tę wielką moc Guru. A potem automatycznie, przekazujecie tę moc innym; nie musicie tego wypracowywać. Po prostu w waszej obecności, ludzie mogą uzyskać tę moc pełnej satysfakcji. I jest to zbawienie dla was i dla innych.
+
+Tak więc wszystkie problemy, które napotyka się podczas podróży wznoszenia się na szczyt, są skończone i teraz jesteście przesiąknięci błogością niebiańskiego spokoju i radości. Dlatego nazywa się to Kaivalya, oznacza tylko, wyłącznie błogosławieństwa. Widzicie, oznacza to, że nie ma innego słowa do przetłumaczenia tego. Nie ma innego sposobu na wyjaśnienie tego.
+
+To jest stan. To Stithi. To jest stan, w tym stanie musicie wzrastać i wiecie o tym, że jesteście w tym stanie. To bardzo niezwykła rzecz, że kiedy osiągniecie ten stan, nie musicie o nic prosić. Wszystko tam jest, a wy jesteście bardzo usatysfakcjonowani. Mogę dalej mówić o tej specjalnej mocy, ale myślę, że cokolwiek powiedziałam, medytujcie o tym.
+
+I wy wszyscy jesteście w stanie osiągnąć ten stan. Ten stan całkowitego spokoju i radości.
+
+Niech was Bóg błogosławi.
