ba0be237a2146cfd81cf0363924e4fe9f1dc55a1
amrutadotorg <amruta.org@gmail.com>
2020-04-19 10:00:22 +0200

    new

---
+++ 4766.pl
@@ -0,0 +1,131 @@
+Rozmowa z Yoginami o Wirusach i Infekcjach
+
+01.12.1987 Pune, Indie
+
+(wersja 1 nieweyryfikowana z oryginałem z braku nośników audio i video)
+
+Więc na jakikolwiek sposób warunkujemy samych siebie, staje się to rodzajem naszej podświadomości. Na przykład robimy coś cały czas, więc to się staje naszym uwarunkowaniem, ponieważ powtarzaliśmy to tak wiele razy, staje się to uwarunkowaniem i jest również, jak to nazywacie - nawykiem. I nawyki również są dane przez naszą podświadomość.
+
+Więc możecie powiedzieć, że to jest najbliższe teraźniejszości. Nawet siedząc tutaj, załóżmy, że wezmę coś, co jest bardzo zanieczyszczające, a natychmiast będę wrzucona w taką głębię podświadomości, w obszar kolektywnej podświadomości, że natychmiast coś z kolektywnej podświadomości mnie pochwyci i następnym razem będę musiała to mieć.
+
+To jest to, co się dzieje z narkotykami. Jak tylko raz weźmiecie narkotyki, już to macie. To wrzuca was tak bardzo w ten obszar, że pozostajacie pod wpływem czegoś, co nazywamy bucami. I wtedy musicie...
+
+Teraz, co my mamy w kolektywnej podświadomości? W procesie ewolucji było wiele rzeczy materialnych, które pojawiły się na tej ziemi i były wyrzucone do tej kolektywnej podświadomość jako bezużyteczne. Tak jak obecnie macie plastik. Podobnie mieliśmy wiele takich rzeczy, które były wyrzucone z procesu ewolucji i trzymane po jednej stronie.
+
+Potem mamy inną rzecz, którą nazywamy roślinami. Jak pewne rośliny, które mamy, powiedzmy tytoń. Zatem ten rodzaj roślin, które mamy teraz, zamierzamy wyrzucić poza obieg. W ten sam sposób było wiele innych rzeczy, które były nadużywane i były usunięte z obiegu, one też tam są.
+
+Następnie po tych roślinach mamy małe zwierzęta, mikro zwierzęta, które również były wyrzucone i trzymane w kolektywnej podświadomości. Następnie na szczycie tego mamy istoty ludzkie, lub najpierw możemy powiedzieć zwierzęta, większe zwierzęta. Wśród nich również tak wiele wypadło z obiegu, one też wciąż tam są.
+
+Po nich idą zwierzęta, niektóre z nich powinny być na wyższym poziomie, ale są ambitne i wciąż pełne pragnień.
+
+Istnieje takie uwarstwienie tej podświadomości. Więc wszystkie te problemy, z którymi się borykamy, które nazywamy ekologicznymi, przyszły z tej kolektywnej podświadomości. A wiecie, że ‘pathos’ oznacza ‘cierpienie’, czyli cokolwiek, co przynosi cierpienie po lewej stronie.
+
+Możemy powiedzieć, że one są właściwie, niekoniecznie białkami, niektóre nawet nie są białkami, ale one również rosną. One są zwane.... Te białka oczywiście, grzyby i wszystkie takie są białkiem.
+
+I teraz mamy również poza tym pewne zwierzęta.... Egipcjanie mieli te same zwierzęta, tego egipskiego psa czy coś takiego. Francuzi są w tym bardzo dobrzy. Zwrócili się ku Egipcjanom, ponieważ oni tak dużo piją, jedzą różnego rodzaju sery i te wszelkiego rodzaju zepsute rzeczy, a potem kierują się naturalnie do Egipcjan, ponieważ oni są po lewej stronie. Więc to bardzo lubią, więc to pochodzi od Egipcjan, ta przeszłość.
+
+Zatem mamy tak wiele rodzajów rzeczy przechowywanych już w naszej istocie. Wszystkie te rzeczy w niej są. To bardzo trudna sprawa. Mam na myśli to, że nic nie ginie na tym świecie. Nic nie jest niszczone. Ale jest umieszczane w różnych, odmiennych przedziałach. A my nie powinniśmy wchodzić do tego przedziału, który nie jest nasz. A dzieje się tak, że zaczynamy zmierzać w jego kierunku. I robimy wiele bezsensownych rzeczy.
+
+Ostatecznie, są tam również istoty ludzkie, które są martwe i które tam przebywają. Wielu ludzi, którzy za dużo piją, mogą pić nie tylko z powodu ich własnych problemów. Jakiś rodzaj buca musiał się do nich dostać, więc tym który pije musi być buc.
+
+Również ci ludzie jak widzicie, ci straszni ludzie i cały ten kram. Ich niewinność jest kwestionowana. To wszystko stąd pochodzi, że oni dojdą do takiego stopnia, żeby odkryć, iż pewnego rodzaju buc w nich siedzi.
+
+Ale większość tych kryminalistów, którzy popełniają przestępstwa wszelkiego rodzaju są najczęściej w nadświadomości. Są bardziej ludźmi w nadświadomości, tymi którzy potrafią działać. Więc ludzie, którzy cierpią są lewo stronni, są ludźmi, którzy cierpią, ale oni są bardzo przebiegli, bardzo szczwani i jest bardzo trudno ich na tym przyłapać.
+
+Oni są skrajnie przebiegli. Znajdują sposoby i metody.
+
+Możemy powiedzieć, że obecnie sytuacja Sahaja Yogi w Anglii jest bardzo podobna. Bardzo szczwani ludzie. Powiedzcie im, by podążali tą drogą. Powiedzcie im, by tak postępowali. Niemożliwe. Sytuacja jest czasami niemożliwa. Ale wtedy oni cierpią i wtedy rozumieją.
+
+A druga strona to ci, którzy są agresywni. I oni są agresywni względem innych. Oni sprawiają kłopoty innym i ich torturują. Tacy mogą również spotkać pewnego rodzaju buca z nadświadomości i mogą stać się naprawdę agresywni, przykładem może być ktoś upijający się. Więc on może się stać bardzo trzeźwą osobą, bardzo miłą, bardzo słodko mówiącą a pewnego dnia może zacząć bić ludzi, bić żonę, bić dzieci. To zależy od rodzaju buca jakiego złapał.
+
+A te wirusy, które są białkami i nie białkowe również, i tak mikroskopijne, że nie możecie ich pojąć, więc jak my możemy próbować je sklonować? Proces ten nazywamy klonowaniem. Polega on na przyłączeniu ich do czegoś takiego jak enzymy. Enzymy, niektóre enzymy mogą dbać o pewnego rodzaju wirusy. Nie wszystkie enzymy potrafią opiekować się tymi wirusami.
+
+Jest tak wiele z nich, tak wiele tych wirusów, że znaleźć ten właściwy enzym dla odpowiedniego wirusa jest bardzo trudno. A ludzie dzisiaj eksperymentują tak szaleńczo z tym klonowaniem enzymów z tymi [wirusami]. Teraz, przeciwciała robią dokładnie to samo, one są jak enzymy, tylko tyle, że one starają się wysyłać enzymy, które kopiują i które usuwają wirusy.
+
+Czasami przeciwciała mogą być zniszczone na samym początku, tak jak w przypadku ludzi, którzy cierpią na AIDS, ponieważ sam Ganesha jest pokonany. Ganesha, który jest Ganapati, co znaczy, że on jest mistrzem wszystkich ganas, czyli wszystkich przeciwciał w nas - jest pokonany. A jeżeli wykorzystaliście już wszystkie przeciwciała, w tym znaczeniu, że jeżeli ich przewodnik jest zniszczony, one wszystkie są zniszczone, więc jak możecie mieć jakąkolwiek odporność?
+
+Więc cała ta zagadkowa, bezsensowna, całkowicie przebiegła głupota, w którą ludzie popadają jest dlatego, że oni są przywiązani do lewej strony. Wpadają w lewą stronę, potem tak to przepracowują i wtedy cierpią.
+
+Teraz co do tych nowych aktywności w tej nowej działalności klonowania - oni mają wiele badań w toku.
+
+Ludzie starają się obserwować szczury, widzicie, oni aplikują im wirusy, potem pobierają enzymy, które je zwalczają, robią to, robią tamto, ponieważ uważają, że mają wirusa w szczurze (a szczur jest Ganeshy i jest w porządku).
+
+I w ten sposób, będzie emitowane więcej enzymów zwalczających te choroby, które będą zwalczać te wirusy. Oni także [mają] dostępne mikro-enzymy, więc będą przygotowywać więcej enzymów tego typu. Ale klonowanie jest problemem. Jak je klonować? Jak odkryć, który jest odpowiedni dla którego? Więc te [...] zajmujemy się w dzisiejszych czasach na świecie tak, że mają laboratoria wszelkiego typu, prace badawcze wszelkiego typu, z jednym celem: by odkryć klonowanie. I to jest jak traktowanie liścia różnymi, różnorodnymi lekami by odkryć, który działa.
+
+Ale czym innym jest Sahaja Yoga. W Sahaja Yodze unikacie tego wszystkiego w taki sposób, że wyciągacie swoją uwagę z kolektywnej podświadomości, z przeszłej podświadomości, z teraźniejszej podświadomości, z dzisiejszej podświadomości - do centrum. Ale do tego trzeba być zdeterminowaną osobą. Jej siła woli musi być mocna. A wszyscy lewostronni ludzie mają bardzo, bardzo słabą siłę woli.
+
+Róbcie co chcecie a oni i tak wrócą do swoich nawyków. Próbujcie jak najlepiej. Powoli, w sposób przebiegły, widzicie. Wszystkiego tego oni nie rozumieją, nazwą to zbiorem swoich wrogów. W rzeczy samej, wpadają w takie kłopoty, że będą cierpieć, cierpieć i cierpieć.
+
+Więc teraz, ta teoria, że musimy cierpieć, pochodzi z ludzkiej inteligencji. Ale nie ma rozwiązań. Teorie jak ta, że musimy cierpieć i cierpienie jest boskie. Tak jak musieliście słyszeć o Żydach i chrześcijanach, jak cierpienie jest im oferowane. I oni wszyscy stali się lewostronni. Ale potem są pod wpływem buców i wtedy stają się prawostronni.
+
+Więc zobaczycie, że teraz Żydzi, którzy wycierpieli tak wiele z rąk Hitlera, stali się skrajnie agresywni. A chrześcijanie, którzy powiedzieli, że będą cierpieć, wszyscy cierpieli przez nich, wszyscy na świecie. Gdziekolwiek nie poszli z Biblią i bronią w dłoniach, widzicie co uczynili. Więc obie ścieżki, mówiące o cierpieniu czynią innych cierpiącymi.
+
+Więc również nadchodzi kamuflowanie, ponieważ oni są bardzo przebiegłymi, oszukującymi ludźmi stwarzającymi problemy. Nie mogą patrzeć na dwoje ludzi szczęśliwych, radujących się.
+
+Wszystkie te rzeczy składają się na waszą wrażliwość. Dostajecie się w pewien rodzaj... Teraz są tam straszne choroby. Mooladara jest atakowana. AIDS, wszystkie upośledzenia mięśniowe, nie macie temperatury.
+
+Z lewej strony nigdy nie macie temperatury. Nigdy, przenigdy nie dostaniecie ataku serca, nigdy. Możecie trzymać aparaturę w szpitalu psychiatrycznym, ale oni nigdy nie dostaną ataku serca, dopóki nie nastąpi jakiś rodzaj szoku, jakiś wypadek czy coś takiego. Oni nigdy nie mają temperatury. W przypadkach raka nigdy nie mają temperatury.
+
+A kiedy Mooladhara jest wyczerpana, pierwszą rzeczą jaka się wydarza jest to, że dostajecie chorobę, którą znacie jako AIDS, a także macie wszelkie choroby, które mają coś wspólnego z mięśniami. Mięśnie zaczynają słabnąć i tym podobne.
+
+Ostatnio w Londynie leczyliśmy taką osobę, która nie mogła chodzić ani normalnie usiąść, która nie mogła usiąść na ziemi. Ale kiedy został wyleczony siedząc tam przez piętnaście minut z ręką skierowaną w moją stronę, z lewą ręką i światłem tutaj, on po prostu podskoczył. Ale znowu staje się... One [negatywności] się tu kręcą. Więc znowu nie mógł podnieść nogi. On podskoczył, w porządku.
+
+Więc teraz trzeba zrozumieć, że musimy być bardzo wymagający co do waszej Mooladhary. Przypuśćmy, że ktoś ma zabucowaną żonę i oni ze sobą żyją, on może to załapać. Jeżeli masz zabucowanego męża też możesz to załapać. To są wszelkiego rodzaju rzeczy, które można załapać. Rzeczy strasznego rodzaju.
+
+Teraz podążacie za tym. Ludzie popadli w inne nawyki. Powiedzmy, alkohol. Alkohol, jak wiecie ma różne wibracje, w zależności od nawyków danej osoby. Jeżeli osoba jest normalnie bardzo cicha, po alkoholu stanie się bardzo gniewnie pobudzona. A jeżeli jest typem bardzo gniewnym, staje się... To jest bardzo zabawne. Ponieważ oni poruszają się z jednego krańca na drugi. I ten alkohol jest bardzo niebezpieczny, ponieważ wykańcza wam wątrobę, wykańcza waszą świadomość, czyni was bardzo ociężałymi, wasza uwaga jest rozproszona... I ostatecznie, będziecie bardzo zaskoczeni, że wszystkie te relacje, o których Freud mówił i wszelkie nadużycia stąd się biorą. Ponieważ alkohol działa na osobę w taki sposób, że ona traci całą swoją dharmę. Nie mają żadnej dharmy. Nie rozumieją matki, nie rozumieją siostry. Stają się opuszczonymi osobnikami. A kiedy stają się opuszczeni, stają się bezwstydni.
+
+Kiedy staje się bezwstydny, to powiada: "Co w tym złego" idzie w tym kierunku i zaczyna niszczyć siebie samego. To właśnie dlatego nazywamy takie społeczeństwo dekadenckim. Ale ten rodzaj społeczeństwa zaczyna być niszczony stopniowo, ponieważ nie ma tam dharmy. Żadnej dharmy. I oni tylko piją i nie wiedzą nic o tym co czynią. Nie ma żadnego sensu w ich głowach. Dlatego, bardzo mocno... całkowicie. Bez wątpienia... narkotyki.
+
+Teraz, narkotyki uderzają w wasze lewe Nabhi albo w prawe. Te, które uderzają was w lewe Nabhi mogą was sprowadzić bardzo głęboko w dół. I czasem możecie być tacy agresywni, pomimo tego, że wzięliście te lewostronne [narkotyki]. To zaskakujące. Po prostu, widzicie, że nagle jesteście osłupiali. Osoba może się stać szalona, wszystko może się wydarzyć osobie, która bierze narkotyki. Można się stać całkowicie ...
+
+Widziałam również jak wszyscy uczniowie Rajneesha byli szaleni, byli całkowicie szaleni. Odchodzili od zmysłów. Byliśmy w stanie wyleczyć tylko jedną osobę, chociaż do pewnego stopnia. Powiedziałbym w 100% w porządku, możecie nawet powiedzieć, że on jest teraz na 75% w porządku.
+
+Więc możecie sobie wyobrazić jak ważna jest Mooladhara. Ilość uwagi, którą dajemy naszej Mooladharze, jest naprawdę szalona. Nie ma potrzeby.
+
+W Anglii na życie małżeńskie jest pierwszy rok, w drugim roku staracie się o rozwód, w trzecim go macie. Nie ma żadnego podtrzymywania, racjonalnego stopniowego zrozumienia. Potem nagle oni zrywają. Żona wchodzi mężowi na głowę albo mąż wchodzi na głowę żonie... nigdy nie są zgodni, nigdy nie są razem, możecie dostrzec, że nie ma żadnego zrozumienia.
+
+Więc wszelkie te perwersyjne pomysły przyszły do nas z kolektywnej podświadomości robienia perwersyjnych rzeczy wszelkiego rodzaju. Nie możemy pojąć sposobu, w jaki ludzie się zachowują.
+
+Ktoś Mi kiedyś mówił, że było to postrzegane jako coś wielkiego, bardzo religijnego to, że kobiety poszły; siostry i matki spały na zewnątrz, zachorowały i umarły, więc stały się świętymi. Teraz każdy może. We Francji. I to była bardzo powszechna sprawa.
+
+Nawet w Rzymie, sposób w jaki ludzie są nagabywani by cierpieć jest jak... Wszędzie. I te wszystkie głupie pomysły idą z lewej strony, te które chcą byście cierpieli. I wy tworzycie męczeństwo z tego cierpienia.
+
+I w całości patogiczne... odkryjcie, że wszystkie te różne, różne wirusy znajdują się w nas.    I kiedykolwiek wpadamy w ... nie musimy wpadać w te wszystkie straszne wirusy. Ale kłopot w tym, że istoty ludzkie nie mają ani krzty siły woli. Są bardzo łatwo kuszeni i chcą robić coś co jest ... "Zobaczmy co się stanie". Jeżeli macie swoją własną godność i macie swoją siłę woli nigdy, nigdy im nie ulegniecie, ponieważ wiecie, że to wszystko są bardzo niebezpieczne rzeczy, których nie powinniście robić.
+
+A jeżeli powiem: "W porządku nie bierzcie tego sera" to wciąż Sahaja yogini powiedzą: "Nie, ale ja to lubię".
+
+Czemu mówię, nie bierzcie tego? Potem powiem, w porządku. Ale to powinno być po prostu odrzucone. Ponieważ wy wiecie co to jest, to jest wirus. Wtedy zapraszacie do siebie problem. Jeżeli chcecie zaprosić kłopot do siebie, proszę bardzo, róbcie tak. Ale wtedy nie można was nazywać Sahaja yoginami.
+
+Więc jedyne co jest złego w tym wszystkim to to "co w tym złego?" Przede wszystkim oni są całkowitymi ignorantami a po drugie "co w tym złego?" Nie macie żadnego prawa, by siebie niszczyć. Bóg was stworzył, wy siebie nie stworzyliście. Kim wy jesteście, by siebie niszczyć?
+
+Nie macie żadnego interesu. I to jest to, co powinno być zrozumiane przez Sahaja yoginów, że nie macie żadnego prawa, by siebie samych niszczyć i samych siebie psuć.
+
+Musi zrozumieć, że teraz wy jesteście... jeżeli naprawdę rozumiecie, w ten sposób, że nazywam to częścią tego, co jest Bodha – to jest, to się staje częścią waszego układu nerwowego, że jesteście yoginem, którego żaden wirus nie może zaatakować. Co to za wirus?
+
+Nic was nie może zaatakować, nic was nie może niepokoić. A nawet jeżeli was zaatakują, to przyjdzie na chwilę i odejdzie. Nawet jeżeli zaatakują, to przyjdzie i odejdzie. Ale to nie zostanie w waszym ciele. To nie może długo zostać.
+
+Teraz, jakieś pytania?
+
+Pytanie: ... sny....
+
+Odpowiedź: Jeżeli Mnie widzicie, wtedy idziecie do Sushumny. Ale jeżeli widzicie rzeczy wszelkiego rodzaju na swój temat!? Potem nagle on jest atakowany przez coś tam. On zapomina. A potem on wraca i mówi: "Och jak mogłem to zrobić?" Ale w tym czasie, wy nie jesteście sobą.
+
+To również może wydarzyć się z ego: "Zrobię to, zrobię tamto".
+
+A najważniejszą rzeczą jest to, co ma być w waszym sercu. Najważniejsza rzecz jest zawsze w waszym sercu. Więc jeżeli ustanowicie to w waszym sercu, cały czas, to jest ustanowienie. Każdy, widzicie, wciąż daje problem, ten problem, tamten problem. To jest bardzo proste - tylko włóżcie to do serca. Takie proste.
+
+Jeżeli powiecie: "Matko, przyjdź do mojego serca". Jestem tam. Powiedzcie: "Przyjdź do mojej głowy". Jestem tam. Jestem do waszych usług. Ci Bogowie to uczynili.
+
+Ale ta aktywność ego! Gdzie oddzielacie siebie od swojej Matki, zaczynacie myśleć: "Och, przybywałem do Ciebie, czciłem Ciebie. Jestem tym". Co to jest? Jesteście częścią jednego ciała. Czy jeden palec może stać się ważniejszy niż inny? [Uważacie] że on może ...
+
+Więc tego rodzaju aktywność ego też tam jest: "Jestem tym lepszym, chcę być lepszym Sahaja yoginem niż inni".
+
+A lepszy Sahaja yogin jest [jeszcze] w porządku, ale by być lepszym od innych: "Jestem czymś wielkim, wiem wszystko. Wiem to". To jest również powszechne.
+
+Lepszy Sahaja yogin jest w porządku, ale żeby być lepszym od innych na zasadzie:
+
+któregoś dnia, mówiłam... jemu samemu. Widzicie on nie pozwolił nikomu do Mnie mówić. Ktokolwiek mówi .... czuję się taka zakłopotana. Mówiłam mu, żeby tego nie robił. Wiem jak powiedzieć. Ktokolwiek coś mówi, on tylko ... Więc ... nie robić tego. Ale to wszystko jest prawostronne. Oczywiście on jest liderem, w porządku, ale Ja bym chciała do nich wszystkich przemawiać. Jeżeli ktoś jest niegrzeczny lub śmieszny, wtedy to jest ok. Ale jeżeli ktoś do Mnie mówi, nie powinniście go powstrzymywać. Mam na myśli, że nie jesteś jedynym rzecznikiem. Oni wszyscy są moimi dziećmi.
+
+A także to liderowanie, czym to jest? To jest mit. Każdy ma takie problemy, ale jak tylko się staniesz liderem musisz być bardzo ostrożny ... bardzo wielkiego charakteru.
+
+H.H. Shri Mataji Nirmala Devi
