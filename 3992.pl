daaa8799c3345ac800dace4645b810680d085656
amrutadotorg <amruta.org@gmail.com>
2020-04-28 10:01:00 +0200

    new

---
+++ 3992.pl
@@ -0,0 +1,69 @@
+Puja Powitalna w Chalamali
+
+21.12.1986, Alibag (Indie)
+
+(Część wykładu wygłoszona w języku angielskim, zaczyna się od 7:43 minuty)
+
+(Napisów do części tłumaczonej z marathi brak)
+
+Kłaniam się wszystkim Sahaja yoginom. W tym pięknym otoczeniu, wielu z was zastanawia się, dlaczego Bóg stworzył te piękne rzeczy. Ponieważ wy ludzie mieliście przybyć na tę Ziemię i cieszyć się pięknem - to jeden z powodów. A teraz Bóg czuje się ogromnie wypełniony radością i satysfakcją, i pewnego rodzaju spełnieniem Swojego pragnienia.
+
+Dlaczego Bóg stworzył ten piękny Wszechświat? - było pytaniem zadawanym przez tysiące lat. Powód jest bardzo prosty do zrozumienia - to piękno, które powstaje, nie może zobaczyć siebie. W ten sam sposób Bóg, który jest źródłem piękna, nie może zobaczyć własnego piękna, tak jak perła nie może wejść w siebie, aby zobaczyć swoje piękno, tak jak niebo nie może zrozumieć swojego własnego piękna, gwiazdy nie mogą zobaczyć swojego piękna, słońce nie może ujrzeć swojego blasku, podobnie Bóg Wszechmogący nie może ujrzeć własnego istnienia. On potrzebuje lustra i dlatego stworzył ten piękny Wszechświat jako Swoje lustro.    W tym lustrze stworzył piękne rzeczy, takie jak słońce, a teraz słońce też musi zobaczyć swoje odbicie.
+
+Zatem stworzył te piękne drzewa, aby zobaczyć, że kiedy świeci, one rosną tak dobrze i wyglądają tak zielono. Potem stworzył ptaki, by wstały wcześnie rano, aby powitać słońce, Tak więc Słońce wie: "Tak, jestem tutaj. Istnieję" i stworzył te piękne jeziora, aby te drzewa zobaczyły swoje odbicie, a jeziora, aby czuły swoje istnienie, gdy fale zaczynają się na nich poruszać. A następnie stworzył istoty ludzkie. Ale ludzie także nie mogą zobaczyć własnego piękna, własnej chwały. Dlatego tkwią w ciemności, są ignorantami. Nie mogą zobaczyć, co istnieje w nich samych. Więc gonią za czymś, co jest absolutnie przeciwko nim, co jest brzydotą, co jest samo-destrukcją. A walka, aby uzyskać rzeczywistość jest tak wielka, że w rezultacie sami siebie niszczą.
+
+Jak np. ludzie zażywali narkotyki, prowadzące do wszystkich złych nawyków - mówi się, że tutaj ma miejsce ogromny przemyt - ludzie podejmują wszystkie takie wysiłki, ponieważ nie wiedzą, jakim są pięknem.
+
+Więc teraz trzeba dla nich stworzyć lustro, aby ujrzeli jacy są piękni. Ich lustrem jest ich Duch. Tego Ducha trzeba wprowadzić do ich świadomości, do ich uwagi. Kiedy [Duch] pojawi się w ich uwadze, wtedy będą mogli zobaczyć swój obraz. Teraz ta uwaga, która pojawia się jako uwaga oświecona, nazywa się w języku sanskryckim 'chidabhas' jest przejawem oświeconej uwagi. Ale mówi się również, że uwaga ma bardzo ulotną naturę, lub możemy powiedzieć, znika z każdą chwilą. Kiedy tak się dzieje, co należy wtedy robić? W tym celu musicie się uspokoić poprzez medytację, słuchając oświeconego wykładu, widząc dobre strony życia, bez myślenia o tym, widząc także dobroć innych, wtedy zaczniecie dostrzegać dobro także w sobie.
+
+Dlatego zawsze mówię, że nie należy czuć się winnym, ponieważ czujecie się winni czegoś, co jest tylko iluzją. Czujecie się winni z powodu tego, że macie cień. To jest coś takiego. Więc jeśli wiecie, że jesteście pięknymi istotami, a to, że macie cień, nie ma nic wspólnego z wami ani z waszym pięknem. Jeśli widzicie własne piękno, wtedy będziecie zaskoczeni, że wszystko, co daje radość jest w nas, wszystkie piękne rzeczy, które widzimy na zewnątrz.
+
+Ale teraz lustro nie jest w porządku, dlatego musicie dostrzec piękno w naturze. Kiedy jesteście jednością ze sobą, wtedy widzicie całą tę naturę w sobie, wszystkie dające radość cechy wewnątrz samych siebie. Więc chciałam, żebyście przyjechali właśnie do tego miejsca, aby nauczyć się, w jaki sposób Bóg stworzył ten piękny obszar dla was ludzi, abyście się nim cieszyli i byli tutaj.
+
+Niech was Bóg błogosławi.
+
+[Tłumaczenie z Marathi -  tekst po weryfikacji 03.03.2020]
+
+Kłaniam się wszystkim Sahaja yoginom. Najpierw będę mówić w języku angielskim a potem w Marathi. Mówiłam im, że wielu ludzi pyta: "Po co Bóg stworzył ten świat. Po co ten świat był potrzebny?" Powodem jest to, że Bóg jest źródłem piękna, radości i miłości, a On nie może zobaczyć samego siebie.
+
+On nie może zrozumieć, jak wielkim On jest źródłem. Podobnie wy Sahaja yogini jesteście również jego [piękna] źródłami. Stąd Bóg stworzył takie wielkie zwierciadło. On chce widzieć swoje własne piękno w tym zwierciadle. On czuje się zadowolony patrząc w to lustro, ale On chce zobaczyć w tym lustrze coś więcej.
+
+On chce zobaczyć, czy to lustro obudziło się w istocie ludzkiej, czy to piękno przeniknęło istotę ludzką, którą On stworzył, czy istota ludzka jest świadoma swojego piękna, jak wiele jakości otrzymała, jak jest ważna, jak jest wspaniała, czy ona jest warta zrozumienia tego.
+
+Święty Tukaram powiedział: "Chociaż jestem mniejszy od najmniejszego, jestem wielki jak niebo". Osoba, która uświadomiła sobie to o sobie samej, nie będzie marnowała swojego życia na trywialne, błahe sprawy. Jak wiemy, w naszym kraju takie brudne praktyki stały się modne.
+
+Teraz o alkoholizmie. Jeżeli ktoś zaleca niepicie trunku, nikt tego nie akceptuje i połowa ludzi może się zdenerwować i wyjść. Ale gdy to lustro, jakim jest wasz Duch jest przebudzone, wy widzicie siebie w tym lustrze. W chwili, gdy tylko uświadomicie sobie, że byliście o krok od spożycia czegoś bardzo brudnego, co wam tylko zaszkodzi, sami tego zaniechacie.
+
+Po wtóre, po przebudzeniu tego lustra, egoista w was może zobaczyć swoją własną arogancję, z jaką rani uczucia innych i nęka innych. Nie jesteście w stanie w inny sposób dostrzec tych rzeczy, czegokolwiek byście nie czytali, jakkolwiek byście nie intonowali imienia Boga, jakkolwiek byście nie tańczyli w Jego imieniu.
+
+Tylko kiedy wyraźnie widzicie światło Ducha i zaczynacie przyglądać się swojemu Duchowi, uświadomicie sobie, że to, co macie zrobić, to przeciwstawić się samemu sobie. Jestem takim szlachetnym człowiekiem, jestem tak wspaniały, Bóg uczynił mnie tak wspaniałym! Nie jestem zwierzęciem. Jako istotę ludzką obdarzono mnie dobrą inteligencją, obdarzono wolnością, która jest wolnością najwyższą i powinienem to osiągnąć.
+
+Zatem czemu miałbym popaść w niewolę absurdalnych rzeczy, niewolę nałogu? Odkąd jestem tak niezależny i żyję w technice Ducha "Sva – tantra" - niewoli nie powinno tu być. Stąd Shivaji Maharaj kładł nacisk na utożsamianie się ze Sva- Dharmą, co oznacza utożsamianie się z Dharmą Sva. "Sva" oznacza wasze zwierciadło, którym jest wasz Duch i musicie go osiągnąć.
+
+Teraz tak wiele śmiesznych poglądów jest rozpowszechnionych w naszych wioskach, że czasami jestem zadziwiona. Pewna liczba ludzi myśli, że Bóg wchodzi do czyjegoś ciała i wydaje dźwięk podmuchu powietrza. Wiele kobiet myśli, że Boginie wchodzą w nie. Ale chcę wam powiedzieć, że posiadanie Bogini w ciele nie jest łatwą rzeczą.
+
+Jest moc Bogini, poprzez którą Kundalini jest obudzona. Jeżeli szanujecie kobietę jako Boginię, w którą wchodzą duchy, jesteście przeciw Bogu. To są duchy i ta kobieta fizycznie i psychicznie cierpi będąc ich medium [przekaźnikiem]. Nie tylko to, ale wszyscy członkowie jej rodziny podupadają. Więc nikt nie powinien łatwowiernie się temu poddawać.
+
+Podobnie w każdej wiosce, jest taka lub inna osoba, która wykonuje takie czynności. To jest zwłaszcza zauważalne w rejonie Konkanu. W tym roku odkryłam, że nie jest tak źle. Oni opowiadają absurdalne historie, że duch przybył od tej osoby i ta osoba w jakiś sposób hipnotyzuje ludzi. Potem ogłupia tych zahipnotyzowanych ludzi i wyciąga pieniądze. Tutaj to jest powszechna praktyka.
+
+Potem ktoś przybywa jako ksiądz i żąda pieniędzy w imieniu Boga. Bóg nie rozumie pieniędzy jak i nie pragnie waszych pieniędzy. Bóg chce byście trzymali zwierciadło waszego Ducha w waszych dłoniach i patrzyli na swój własny kształt. Ale jest powiedziane, że gdy posiadacie zwierciadło w swoich dłoniach, to macie o tym raczej niejasną świadomość. Dla wyraźnej idei i ustabilizowania się w niej, osoba musi podtrzymywać to poprzez medytację.
+
+Więc najpierw miejcie obudzenie Kundalini, a potem wraz z medytacją i zrozumieniem waszej odpowiedzialności i wolności zasiądźcie na swoim tronie. Nie jesteśmy zwierzętami, lecz istotami ludzkimi. Nie powzięliśmy naszych żyć, aby je zmarnować na tak absurdalne aktywności. Jakkolwiek do dzisiaj nie szanowaliśmy siebie. Ale poznając swojego Ducha, osoba natychmiast poznaje siebie i wszystkie jej złe cechy odpadają.
+
+Kiedy zaczęłam głosić Sahaja Yogę w Anglii, przyszło siedem takich osób, które miały mnóstwo uzależnień. Ale kiedy dostały swoją Samorealizację, tego samego dnia były wolne od swoich nałogów. Więc teraz powinniście podjąć decyzję, by nie akceptować jakiejkolwiek niewoli. Bycie uzależnionym jest niewolnictwem. Co prawda możecie się z tego cieszyć, ale faktycznie to jest niewola.
+
+Więc powinniście postanowić, że nie będziecie akceptować jakiegokolwiek zniewolenia. A kiedy czynicie takie postanowienie, wasz Duch wyraźnie się przejawia, a rezultaty tego są odczuwalne. W rezultacie tego wszystko to, co jest niepotrzebne odpada. Przed tym my lubimy tylko to, czego nie powinniśmy, co jest złe. Wybieramy tylko to, co jest złe.
+
+Więc mówię wam jako Matka, że nikt nie powinien czuć się źle, więc nie obawiajcie się, jeśli macie jakieś uzależnienia, wraz z przebudzeniem Ducha wszyscy będą wolni od uzależnień. Całe ludzkie ubóstwo zniknie.
+
+Wszelkiego rodzaju bóle, wszelkiego rodzaju problemy fizyczne, psychiczne, emocjonalne, a także problemy rodzinne, powinny zniknąć i to musi się wydarzyć. Więc jesteśmy w waszej wiosce Chalgaon. Ci ludzie, również widzieli czystość tego miejsca. Spotkali się z wami, tańczyli z wami, cieszyli się z wami.
+
+Również wy miło gratulowaliście im i ugotowaliście dla nich pyszne jedzenie. Więc oni prosili: "Shri Mataji, prosimy obdarz ich życiowym spełnieniem". Teraz naszym zwyczajem jest wręczenie prezentu gospodyni miejsca, które odwiedzamy. W związku z tym Pan Harish Chandra Kali, którego znacie, pracował bardzo ciężko, postawił tutaj swój dom, ustabilizował Sahaja Yogę. Jego żona też bardzo mu pomogła.
+
+Więc chcemy podarować jej coś przynajmniej jako wyraz naszej miłości i wdzięczności. Ja również chcę coś podarować wszystkim innym ludziom. To to jest to, co nazywamy błogosławieństwami. Dzięki tym błogosławieństwom macie stać się "Samartha" co znaczy "równy sensowi waszego życia, to również znaczy silny" – przez obudzenie waszej Kundalini Shakti.
+
+Macie również stać się samowystarczalni. Zanim opuszczę to miejsce, chcę zrobić coś dzięki czemu w pełni zrozumiecie swoją własną chwałę, zrozumiecie swoją własną wielkość i będziecie mieli mądrość, która przychodzi z takim zrozumieniem.
+
+A tym samym, podczas mojej następnej wizyty, zobaczę wszystkich ludzi bardzo pięknymi, ze świeżością i nowym wyglądem na twarzach, mającymi piękne miejsca zrobione przez Boga w ich sercach, miejscem, które jest jak w pełni rozkwitłe kwiaty lotosu. Wszyscy macie Moje błogosławieństwa.
+
+H.H. Shri Mataji Nirmala Devi
