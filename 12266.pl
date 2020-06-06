e525511f3e2cdb4edeead7309931cff2dbb32e69
amrutadotorg <amruta.org@gmail.com>
2020-06-06 10:00:40 +0200

    new

---
+++ 12266.pl
@@ -0,0 +1,51 @@
+Odręczne Notatki Shri Mataji - Rady dla Liderów
+
+(W 1992 roku Shri Mataji przekazała Yogi Mahajanowi ręcznie pisane notatki, aby przekazać je wszystkim przywódcom tamtych czasów. On zrobił kopie i wręczył je zgodnie z Jej instrukcjami. To są właśnie te notatki, które napisał wtedy Shri Mataji.)
+
+Aby wyczuć, jak przebiega naturalny proces poprzez zasadę Kundalini, dla tej wiedzy liderzy muszą medytować i podążać za tym, co na ich własny temat dyktują im wibracje.
+
+Po pierwsze, przed medytacją zróbcie siedem razy bandhan.
+
+Miejcie otwarty umysł.
+
+Najpierw znajdźcie swoje uwarunkowania i wasze preferencje i uprzedzenia.
+
+Uprzedzeni liderzy, kiedy nie są głębocy, widzą w innych to, co pasuje ich uprzedzeniom.
+
+By poznać, jak rzeczy się mają, nie używajcie waszych umysłowych projekcji czy rozważań.
+
+Oni działają (poruszają się) w sposób liniowy (linearny), który cofa się pod ich polaryzacją. Nie próbujcie wpływać na działania. Oni mówią: "Chcielibyśmy poruszyć masy do działania". Po prostu zastosujcie zasadę, a proces się zamanifestuje
+
+Żadna umysłowa analiza nie jest potrzebna, jako że nie ma przyczyny ani skutku. Duch, który jest oświecony przez Kundalini wypracowuje wszystko, ponieważ ma wbudowaną zdolność jak ziarenko. Matka Ziemia jest Param [najwyższą] Mocą.
+
+Pozwólcie Boskości to w was wypracować, poprzez waszego Ducha. Pracujecie mniej umysłowo, będziecie tylko instrumentem. Bycie - jest stawaniem się.
+
+To działa poprzez poddanie się: "O Matko, przyjdź do mojego serca, proszę przyjdź do mojej głowy, proszę przyjdź do mojej uwagi". Kundalini wykona dla was tę pracę.
+
+Niełatwo jest poddać przywództwo, bo jest to trudne. Prawdziwa prostota nie jest łatwa dla skomplikowanego umysłu. Lider naucza przykładem a nie wykładem, który nadyma ego i tworzy u liderów Shabda Jalam (sanskrit - sieć fałszywych słów lub idei).
+
+Nie poszukujcie i nie martwcie się o pieniądze czy sławę, ich obu jest mnóstwo w Sahaja Yodze. Widziałam, jak ludzie tracili to momentalnie, w chwili, gdy zaczynali dochodzić do wniosku: "Nic złego się ze mną nie dzieje" - ponieważ oni są liderami. "Jestem całkowicie w porządku."
+
+Tacy są wyrzuceni z Sahaja Yogi przez odśrodkową siłę prawdy i w trzydziestu narodach, gdzie dzwony biły na ich chwałę, nagle zaczęły zapowiadać koniec, pogłoski przeszły w głośne obwieszczenie potępienia.
+
+Ciągłe ingerowanie w grupę blokuje postęp grupy. Jeżeli ktoś nieustająco przysparza kłopotów, połóżcie na niego uwagę poprzez waszą Matkę, a on albo się poprawi albo odejdzie.
+
+Nie martwcie się o to, kto opuszcza Sahaja Yogę. Nie ma miejsca na połowicznych yoginów w Królestwie Boga.
+
+Pamiętajcie, że wszystkie umysłowe projekcje mają biegunowość i w Sahaja Yodze to działa bardzo szybko. Cokolwiek jest z bardzo czystej bieli, bardzo wyraźnie ukazuje na sobie najmniejsze czarne plamki.
+
+Liderzy nigdy nie powinni mówić grupie o ich własnym problemie, życiu czy oddaleniu. Każda taka obsesja jest niebezpieczna dla wzrostu danej osoby, jak i dla danej grupy. Dobry lider nigdy nie powinien popisywać się swoją świętością, czy swoją tak zwaną bliższą relacją z Matką. Nieodpowiednim jest posiadanie swoich faworytów, nawet wasza żona powinna być traktowana jak inni Sahaja yogini.
+
+Nie powinno się u niej zasięgać porad, wręcz przeciwnie, jeżeli ona doradza wam przeciwko komuś lub na rzecz kogoś, powiedzcie jej proszę, że wy wszystko wiecie.
+
+By całym sobą mieć pełne podparcie w Paramchaitanyi i być świadomym grupy, możecie przeniknąć wrodzoną religię Vishwa Nirmala Dharma. Nie ma potrzeby tworzenia stopni w grupie, ponieważ to powoduje rywalizację i zazdrość. Pozostawcie to osądowi Matki. Własny stan świadomości lidera czyni go bardziej otwartym, wrażliwym i chronionym. Stąd czerpie szacunek, forma nie zastąpi treści.
+
+Dobry lider nie jest spektakularny, ale za to jego grupa jest!!!Sława, której szuka, oddala go od zadania.
+
+Lider nie powinien przyćmiewać przekazu. Lider nie powinien mieć ulubieńców.
+
+Proszę, oczyśćcie swoje emocjonalne konflikty. Prowadźcie poprzez dowartościowanie, bez niszczenia i ranienia.
+
+Nie przypisujcie sobie zasługi za pomaganie. Prowadźcie bez zaborczości. Obserwujcie ciszę w grupie i czujcie jej istotę i zainteresowanie. Czujcie zasadniczy nastrój grupy. Skąd wieje wiatr w grupie.
+
+H.H. Shri Mataji Nirmala Devi
