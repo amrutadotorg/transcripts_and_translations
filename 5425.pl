7d8c91a5ed142835e8eec89df8d603f13fd17639
amrutadotorg <admin@amruta.org>
2020-12-24 10:07:10 +0100

    new

---
+++ 5425.pl
@@ -0,0 +1,23 @@
+Czakra Nabhi
+
+Wykład wygłoszony podczas pierwszej tury zachodnich yoginów po Indiach (styczeń-luty 1977). Dokładna lokalizacja nieznana.
+
+Czakra Nabhi jest umieszczona w środku ciężkości każdego człowieka. Jeśli jej tam nie ma, to znaczy, że będzie mały problem, aby mieć ją we właściwym miejscu.
+
+Jest pewien problem, na który cierpi większość z was, może z powodu narkotyków lub problemów nerwowych, czy z powodu wojny, a może jakiegoś szoku dla waszej egzystencji. Na przykład podczas wojny ludzie stracili swoje wartości, ponieważ utracili wiarę w Boga. Czyste kobiety, które wierzyły w czystość, były brutalnie atakowane. Prześladowano bardzo religijnych ludzi, rodziny zostały rozbite, wielu ludzi zostało zabitych, a dzieci, kobiety i starcy zostali rozproszeni. Straszna atmosfera niepewności pokonała wszystkie te narody.
+
+Potem przyszły także obozy koncentracyjne, które zdruzgotały ludzi, ponieważ istoty ludzkie są bardzo delikatnymi instrumentami. Oni są upragnionymi, najwyższymi dziełami stworzenia, a zostali zdominowani przez rzeczy takie jak bomby, które są materią. W ten sposób Duch w człowieku wygasł. Ludzie stracili wiarę w prawość, w miłość.
+
+Następnie zbudowano nowy wzorzec środków bezpieczeństwa. W rezultacie nastąpiła rewolucja przemysłowa, a sztuczne poczucie radości, bezpieczeństwa, miłości, zostało zaakceptowane przez społeczeństwo.
+
+Oto, co człowiek zrobił ze swoją wolnością, ponieważ wojny są tworzone przez człowieka, nie są dziełem Boga. Jednocześnie z tym, narodziły się na tej Ziemi bardzo wielkie poszukujące dusze. Zaczęli szukać poza tymi sztucznymi zabezpieczeniami, takimi jak materializm.
+
+Poszukiwacze nie mieli odpowiednich przywódców, którzy by ich zorganizowali i poprowadzili ich ku właściwej ścieżce [rozwoju]. Więc popełnione przez nich błędy stworzyły przeszkodę.
+
+Tak więc, oprócz przeszkód związanych z ludzką świadomością, dodano tak wiele innych przeszkód. Dodano tak wiele innych przeszkód, co uczyniło dla nich Sahaja Yogę trudnym procesem.
+
+Tylko kraje, które brały udział w wojnie stały się krajami rozwiniętymi. Jako reakcja, nie, to reakcja, oni są jedynymi [materialnie] rozwiniętymi ludźmi, którzy faktycznie biorą udział w wojnie. A ci, którzy tego nie zrobili, są słabo rozwinięci.
+
+Tak więc z jednej strony mamy kraje, które próbują poszukiwać, ponieważ są nadmiernie rozwinięte, ponieważ są zamożne, ale ich poszukiwacze stracili cumy [hamulce], z powodu anty-uczuć. W porządku? Podczas gdy inne kraje nie są jeszcze rozwinięte, więc tam poszukujący wciąż szukają pieniędzy. W tym momencie odkryto Sahaja Yogę, to był ten etap, przez który zapowiadana jest jedność ludzkiej świadomości z Bogiem, przez którą czujecie swoją nieświadomość, Moc Boga, która jest Wszechprzenikająca, która myśli, organizuje i planuje.
+
+Tylko przez Kundalini, Yogę Sahaja, Sahaja Kundalini Yogę, tylko poprzez Sahaja Kundalini Yogę, istoty ludzkie będą ewoluować. Ale ci, którzy mają się rozwijać, muszą mieć nienaruszoną dharmę.
