11ddec26b64c088f46e1f734686cebcc14b4831d
amrutadotorg <admin@amruta.org>
2021-04-07 10:00:35 +0200

    new

---
+++ 44047.pl
@@ -0,0 +1,45 @@
+Prawe Serce i Cechy Przywódcze
+
+Artykuł dr Rustoma
+
+(Nirmala Yoga Vol.5 No. 30, Nov-Dec. 1985)
+
+"Pan Bóg Wszechmogący jest Absolutnym Władcą Wszechświata". Amen.
+
+Ruch w nieświadomości jest zawsze od lewej do prawej do środka. Czakra serca, zarówno lewa, jak i środkowa, reprezentuje boską ochronę i poczucie bezpieczeństwa, które po ludzku przekłada się na ochronę rodziców. W prawym sercu; staje się ona ochroną Ojca i Króla, lub w nowoczesnym ujęciu Przywódcy. Jego bóstwem rządzącym jest Pan Rama jako Inkarnacja Pana Vishnu, podtrzymującego i przenikającego Wszechświat.
+
+Powszechnie wiadomo, że we wszystkich swoich wcieleniach i w swoim stanie poza wcieleniami, Shri Vishnu służy wiecznie potrzebom Wszechświata i swojej Matki, Shri Adishakti, bez wahania i bezinteresownie. Nigdy nie można znaleźć ani jednej próby samouwielbienia dokonanej przez Pana; tylko szczodre oddanie, służbę, poświęcenie się i poddanie Shri Adishakti i Panu Absolutu.
+
+Ten, który jest Panem potęgi bogactwa - zasady Lakshmi - nie bierze niczego dla siebie, ale bezinteresownie rozprowadza wszystko po całym Wszechświecie. W całkowitym odwiązaniu reguluje linie Wszechświata bez osobistego przywiązania. W Sahastrarze, jego Sahasrara jest Sahastrarą Viraty, podobnie w Mooladharze. W Agnyi jest On Shri Mahavishnu, Panem Chrystusem, który rozporządza wszystkimi rzeczamii we Wszechświecie.
+
+Pan Rama jako król zawsze służył i nie szukał dla siebie ani bogactwa, ani chwały, ani komfortu. Jako Maryada Purushottama był przykładem dla swojego ludu i świadomy tego. Dobrowolnie przyjął 14-letnie wygnanie w lesie jak też wygnanie swojej żony - forma i aspekt Shri Adi Shakti.
+
+Poddał się [na polu bitwy] swoim dzieciom, i dzięki temu stał się Ojcem Narodów.
+
+Gdyby ktoś przejrzał całą Ramayanę, nie byłby w stanie znaleźć nawet jednego przykładu, w którym Pan stał się samolubny lub zachowywał się samolubnie, bo całe jego życie było służbą i obowiązkiem, a wspierali Go w tym jego bracia oraz Shri Ganesha tattva [zasada] - której dewizą jest również służba, obowiązek, czystość i wsparcie.
+
+Znaczenie tego jest takie, że jeśli ktoś jest Sahaja yoginem, a jego prawe serce się blokuje, to nie dlatego, że wszystko jest służbą, obowiązkiem, bezinteresownością i oddaniem dla Sanghi - kolektywu: nie "my", ale raczej trzymanie się "ja". Prawostronna negatywność zawsze kończy się w ego: "Mogę to zrobić, ’ja’ jestem tym. Spójrzcie, jaki jestem wspaniały" ale tak naprawdę to Virata jest najwspanialszy, Akbar - porównawcza forma Kabira (wspaniały) - tj. wspanialszy (najwspanialszy jest Absolutem, jest Tym poza wszystkim).
+
+Oba są określeniami Boga jako Viraty w Koranie. Dlatego ten, kto mówi "ja", jestem wielki, bez żadnego odniesienia do Viraty, jest w swej istocie przeciw Bogu i jest bluźnierczy w swojej mowie.
+
+Ale ten, kto mówi: "Jesteśmy wspaniali" lub "Jestem wielki tylko dlatego, że jestem częścią Viraty i Boga Wszechmogącego, i dlatego służę Mu, nie biorąc niczego dla siebie" - jest wielki.
+
+Bo wielkość leży tylko w Oczach Boga, nie człowieka, i tylko On może nadać ten tytuł, w przeciwnym razie jest to daremne, puste, wyimaginowane.
+
+Łatwo jest teraz zobaczyć, czym jest blokada w prawym sercu, szczególnie u Sahaja yogina, jeśli jest on liderem. Sahaja yogin musi być bardzo czysty.
+
+W średniowiecznej Europie istniała opowieść, że Pan Chrystus był jak pelikan, ponieważ wierzono, że Matka Pelikan karmiła swoje dzieci, pobierając krew z własnego serca. Jest to piękna opowieść zrodzona z Nieświadomości, pokazująca, jak na poziomie Centralnego Serca, Omkara przyjmuje cnoty i cechy swojej Matki, Shri Adi Shakti i z "najgłębszej" miłości do Niej karmi Jej dzieci, miłością i krwią własnego Serca.
+
+Przywódca Sahaja Yogi również musi taki być - ponieważ przywództwo nie prowadzi, ale służy krwią serca, a nieczyste serce nie może przewodzić, ponieważ jest jak maszyna bez zasilania i mocy.
+
+Istotą Sahaja Yogi jest bycie narzędziem do przekazywania całej ludzkości i całemu stworzeniu Miłości naszej najświętszej i Boskiej Matki, bez której wszyscy i wszystko traci swój sens.
+
+Prawostronna i nadświadoma negatywność musi zniszczyć Miłość, ponieważ nie może przetrwać z Miłością, ale jest przez nią powalana, niszczona i zdławiona; istotą prawostronnej negatywności jest władza, nienawiść i kontrola - wszystkie działania skierowane przeciwko Bogu. Miłość jest esencją Boga Ojca Wszechmogącego i promieniowaniem Jego Łaski w całym Wszechświecie.
+
+Powszechnie wiadomo, że Pan Rama był głęboko kochany w całym Wszechświecie, podobnie jak Matka Shri Sita i Pan Hanumana: szczerze oddani. Podobnie jak Panowie Lakshmana, Bharata i Shatru-ghna. Poddani Ayodhyi ich kochali i byli również kochani - cała historia to przedstawienie i mieszanie się różnych form czystej miłości i ataków negatywności.
+
+Powyżej prawego serca znajduje się prawe Vishuddhi, a poniżej wątroba - i powszechnie wiadomo, że nadświadoma negatywność uwielbia atakować te dwie czakry.
+
+Więc tylko Miłość i oddanie w prawym sercu, moc Shiva Tattvy w Panu Ramie, może ją przezwyciężyć. Bądźmy tego świadomi i pracujmy, aby oczyścić siebie i Shri Holy Sangha [świętą kolektywność] u stóp naszej Matki, tak jak Pan Chrystus mył stopy swoich uczniów. Amen
+
+Dr. Rustom
