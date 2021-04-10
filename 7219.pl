93d7feeb12eabb08b05f5a1a0679a83ed23bd4d2
amrutadotorg <admin@amruta.org>
2021-04-10 10:00:17 +0200

    new

---
+++ 7219.pl
@@ -0,0 +1,43 @@
+https://youtu.be/7N4gQHrbITY
+
+(Fragment z wykładu: Mowa do Sahaja Yoginów, Warsztaty dla początkujących, 20.03.1983, Sydney, Australia)
+
+W pewnym sensie musimy tak dbać o siebie, żeby zachować dobre zdrowie. Aby zachować zdrowie w Sahaja Yodze, jest tysiąc i jeden rzeczy, które musicie wiedzieć, a które są bardzo łatwe do zrozumienia, i opowiemy wam o nich zupełnie bezpłatnie. To Boskość pragnie, abyście to otrzymali, żebyście się ustabilizowali i radowali się tym.
+
+Więc nie musicie się o to martwić. Te zmartwienia musicie porzucić, po prostu spróbujcie zrozumieć swoje centra [energetyczne]. Teraz wielu z was poczuło chłodny powiew. Ale możliwe, że po jakimś czasie możecie go stracić, nic nie szkodzi. Będziecie poruszać się w górę i w dół, nie ma to znaczenia. To nie znaczy, że to straciliście, wcale nie.
+
+Teraz załóżmy, że spróbujecie metody, prostej metody, i najpierw skierujcie lewą rękę w stronę mojego zdjęcia - teraz możecie spróbować - a prawą rękę do Matki Ziemi, zobaczmy. Prawa ręka do Matki Ziemi, dlatego chciałam mieć to na zewnątrz [budynku], ale teraz nie ma znaczenia, ten deszcz jest dobry dla was i dla rolników, Miejmy to.
+
+Więc teraz połóżcie lewą rękę w ten sposób [na kolanie}, a prawą rękę [do eteru]. Ci, którzy siedzą, mogą ją położyć w ten sposób [do góry]. W ten sposób. Do tyłu. Prawa ręka w ten sposób, dobrze. Nie lewa, prawa. [Yogin]... do ziemi. Nie tak. Do tyłu. Tak, prawa ręka [do eteru]. Teraz, moc lewej strony jest mocą emocjonalną.
+
+Teraz, jeśli macie jakiś problem emocjonalny - powiedzmy, że ktoś zażywał narkotyki, jest on osobą lewostronną. Niektóre narkotyki, jak LSD i wszystkie te prawo-stronne, ale większość narkotyków przenosi was na lewą stronę. Guru są lewostronną częścią. Większość tych problemów to problemy psychiczne, są to problemy po lewej stronie.
+
+Ale problem intelektualistów jest problemem prawej strony, futurystyczni ludzie są prawostronni. Ludzie uczestniczący w wyścigu szczurów są prawostronni, widzicie, ten rodzaj nadmiernej aktywności, tego typu rzeczy. W przeciwieństwie do tych, co zażywali narkotyki, lewostronnych. Więc najpierw spróbujcie z lewą, zobaczmy. Teraz skierujcie prawą rękę do Matki Ziemi. Skierujcie prawą rękę do Matki Ziemi.
+
+Teraz skierujcie lewą w moją stronę. Stopniowo zauważycie, że [wibracje] zaczną płynąć w waszej lewej dłoni. Czy płyną? Połóżcie prawą rękę na Matce Ziemi. ponieważ Matka Ziemia odbiera; widzicie, wy tylko uziemiacie się, połóżcie dłoń na niej, dotykając jej, dotknij, dotknij jej, widzicie, uziemiacie się, to bardzo proste. Teraz problemy z lewej strony łatwo przejdą do Matki Ziemi, to prawda, ale najlepiej jest mieć światło.
+
+Teraz powiedzmy, że jeśli macie świecę, świeca jest najlepsza na problemy lewej strony, świeca jest najlepsza do usunięcia ich. Ponieważ problemy lewej strony, można powiedzieć, są zimnymi problemami, lub problemami wynikającymi z zimna. Widzicie, osoba, która staje się nieaktywna. Teraz zobaczmy.
+
+Po prostu skierujcie lewą rękę w moim kierunku w ten sposób, a prawą rękę w stronę świecy. Będziecie zdumieni, że czasami świeca zaczyna się palić ‘tak’ i strzelać w ten sposób a czasami tworzy taki czarny dym, który jeśli znajduje się obok ściany, zauważycie, że cała ściana stanie się czarna.
+
+To jest bardzo ciekawe widzieć, jak świeca reaguje, ale musicie mieć [moje] zdjęcie przed sobą. I przed fotografią powinna być świeca i jedna świeca w ten sposób, żeby wyciągać [negatywności]. Dobrze, czy teraz lepiej widać? W porządku? Poczujecie, że to schodzi w dół. Poczujecie od głowy, poczujecie, widzicie, nacisk zmniejsza się. Lepiej teraz? W ten sposób.
+
+Połóżcie rękę w ten sposób, do tyłu, nie w moją stronę, w ten sposób, tak, tak. Teraz połóż lewą rękę, jeśli siedzicie na ziemi, lepiej jest skierować w moją stronę, lewą rękę. Nadchodzi, płynie, to działa. To jest jak energia przechodząca poprzez fotografię, spycha to w dół w stronę światła, a światło to spala - światło się budzi, przez was. To zaskakujące, ale tak się dzieje. Czy teraz lepiej? Zobaczmy. Sprawdźmy, poczujemy to. Sami to poczujecie, nie trzeba żadnych potwierdzeń. Sami to poczujecie.
+
+Skierujcie lewą rękę w moją stronę, lewa ręka do Mnie, w ten sposób, tak. Wszyscy to poczujecie. Teraz ludzie, którzy byli u jakichś guru, którzy mają jakiś rodzaj schizofrenicznej postawy, którzy są osobami o zmiennych nastrojach, szalonymi, napastliwymi, wszelkiego rodzaju, co najmniej skrajni, którzy są emocjonalni, niektóre kobiety, które za dużo płaczą, histeryczki, wszystkie te rzeczy można opanować za pomocą tej prostej metody skierowania lewej ręki w stronę zdjęcia a prawej ręki w kierunku świecy. Widzicie to, działa? Poczujecie ruch w swojej głowie, który poczujecie. [Yogin] Tak. Czujecie to teraz. Nie? [Yogin] Więc nie ma tam tego.
+
+Teraz z prawą ręką do góry, ci, którzy biorą udział wyścigu szczurów, widzicie wielkich ludzi, którzy myślą, że muszą zarabiać pieniądze, ścigają się w polityce i wszystko to - kolejne szaleństwo, które mają. Widzicie, to także pewien rodzaj szaleństwa. Więc tacy ludzie, którzy z tego powodu mają wysokie ciśnienie krwi, będą mieli cukrzycę, będą mieli problemy z wątrobą. Teraz, nawet dla jakiegokolwiek organu, widzicie, każdy organ jaki wymienimy, np. serce, w Sahaja Yodze każdy organ ma naturę dwojakiego rodzaju. Jeden jest letargiczny a drugi nadaktywny.
+
+Więc przypuśćmy, że jesteście osobą nadaktywną. Jeśli jesteście osobą nadaktywną, bardzo dużo się uczycie albo myślicie o przyszłości lub planujecie coś, wtedy macie serce, które jest nadaktywne. Albo macie jelito, które jest nadaktywne. Wątroba jest nadaktywna, cały system jest nadaktywny, będziecie mieli wysokie ciśnienie krwi i wszystkie problemy związane z wysokim ciśnieniem krwi, zaparcia, wszystkie te rzeczy idą w parze przy takiej osobowości.
+
+Następnie, inna osobowość może być letargiczna. Osoba, która ma letargiczne serce, może dostać...musi dostać bajpasy lub coś takiego, ponieważ serce nie pompuje. Możecie mieć letargiczne jelito, taka osoba może - cokolwiek zje, od razu idzie do łazienki, coś w tym rodzaju, lub może być letargiczna wątroba. Kiedy macie letargiczną wątrobę, możecie mieć alergie. Alergie są spowodowane letargiczną wątrobą.
+
+Ale jeśli jesteście osobą nadaktywną, jeśli jesteście nadaktywni, wtedy ta sama wątroba, która dawała wam te alergie, daje wam mdłości, złe samopoczucie, migrenę, wymioty i torsje.
+
+Zatem istnieją także dwa rodzaje wątroby. Mamy dwa rodzaje nerek, dwa rodzaje, wszystko ma dwa rodzaje. Jeśli jesteście osobą letargiczną, jeśli macie letargiczny problem, wtedy musicie używać światła. Dla wszystkich letargicznych problemów najlepsze jest światło, należy użyć światła na letargiczne problemy, jest to lewa strona.
+
+A dla prawej strony, możecie użyć Matki Ziemi. Matka Ziemia jest najlepsza albo woda i sól. Woda i sól są bardzo dobre dla ludzi cierpiących z powodu nadaktywności. Teraz, jak to robić? To dobrze dla wszystkich, ale specjalnie dla nich. Musicie położyć obie ręce najpierw w kierunku zdjęcia, obie stopy w wodzie z małą ilością soli.
+
+Teraz, jeśli czujecie wibracje płynące na obu dłoniach, jednakowo, wszystko jest w porządku, nie musicie się martwić. Przypuśćmy, że na jednej ręce czujecie mocniej, a na drugiej słabiej. Ręka, w której jest słabiej, musi zawsze być w stronę zdjęcia. Ręka, która czuje słabiej, musi być skierowana do zdjęcia a druga musi być do góry.
+
+I będziecie zdumieni przepływem [energii], poczujecie wewnątrz sobie te balony poruszające się w ten sposób. I możecie poczuć, że coś porusza się w waszej głowie, jesteście zrelaksowani. I stopniowo staniecie się mistrzem, będziecie wiedzieli, jak poradzić sobie z tym ciałem, z pewnością, staniecie się mistrzem. A kiedy staniecie się mistrzem - ludzie stali się mistrzami w ciągu ośmiu dni, w ciągu ośmiu dni. Kiedy staniecie się mistrzem, będziecie mogli zajmować się również ciałami innych i pomagać innym ludziom. To takie proste.
