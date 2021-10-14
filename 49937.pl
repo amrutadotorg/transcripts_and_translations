6757a081f1431dc33724fc4d473060eca61b9fca
amrutadotorg <amruta.org@gmail.com>
2021-10-14 08:00:15 +0200

    new

---
+++ 49937.pl
@@ -0,0 +1,51 @@
+"Gotowi do wojny?" Zapytała Shri Mataji Nirmala Devi chór Sahaja yoginów, którzy śpiewali hymny bojowe w namiocie w Delhi 10-go lutego 1983 roku. No cóż, czy jesteśmy gotowi? Jaką bronią władamy, jakie są reguły pola bitwy, co z taktyką i strategią?
+
+Broń: 
+
+Są dwa rodzaje broni:
+
+defensywna, obronna (tarcza, zbroja, hełm) ofensywna, atakująca (miecz, włócznia, łuk i strzały)
+
+W czasach Shri Ramy walka była mało skomplikowana: wycelowuję strzałę, strzelam, zabijam...i jakaż to ogromna przyjemność widzieć padającego raksasa. W czasach Shri Krishny znaczenie jakości oręża było olbrzymie. Arjuna nie pokonałby Karny, gdyby Shri Indra nie nakłonił Karny do oddania napierśnika, który otrzymał od Shri Suryi, a który czynił go niewrażliwym na ciosy. A co jest bronią Ludu Bożego w czasach Shri Mataji Nirmala Devi?
+
+Udoskonalone uzbrojenie wojny yoginów dwudziestego wieku jest sprawą zbyt złożoną, aby ją tutaj odpowiednio zaprezentować. Ponadto służby wywiadowcze Sahajlandu nie pozwoliłyby na ujawnienie delikatnych, tajnych informacji o yogińskim systemie obrony, który złośliwe buce próbują stale bezskutecznie penetrować. Pozwolono mi jednak przekazać kilka podstawowych zasad.
+
+Broń defensywna (obronna)
+
+Istota yogińskiego systemu obronnego wyraża się jednym słowem: oczyszczanie! Wciąż od nowa stosujemy techniki Sahaja Yogi, aby oczyścić poszczególne koshas (powłoki) naszego mechanizmu psychosomatycznego, żeby negatywność utraciła władzę i nie mogła znaleźć a naszym systemie żadnego, nawet małego mrocznego zakątka, gdzie mogłaby się ukryć i zaczaić, czekając na doskonały moment, żeby nas zaatakować. Negatywność zna szlak, którym najłatwiej przeniknąć do naszego systemu (lewy Swadishthan, Nabhi, Agnya), ale najbardziej skuteczna jest wówczas, gdy jedzie na fali żądzy posiadania i przywiązania. Zatem Sahaja yogini, którzy to zrozumieli, są wyjątkowo ostrożni i nie pozwalają, aby małżeństwo, rodzina, praca, walka o władzę lub inne tego typu ludzkie relacje stały się dla nich pułapką.
+
+Prawdziwie skutecznym ukoronowaniem oczyszczania jest nieprzywiązywanie się. Tam, gdzie nie ma przywiązania, negatywność nie ma podstawy do okupowania was. Yogin, który jest nieprzywiązany znika, staje się niewidzialny dla oczu negatywności; ona już nie może go znaleźć. Jednakże on sam, może wyszukiwać, znajdować i niszczyć negatywność, jeśli tylko zechce. Taki Yogin dołącza do szeregów jednostki przełamującej Sił Obrony Sahajlandu, dywizjonu bhakta Yogeshwari, którego nazwa pochodzi od zasady, która nim kieruje.
+
+Broń ofensywna
+
+Istotę yogińskiego systemu ataku wyraża jedno słowo: UWAGA! Uwaga (chitta) jest ostrzem świadomości (chit) i im bardziej ta uwaga daje się skoncentrować, tym ostrze jest ostrzejsze.
+
+Sahaja Yoga podaje wiele ćwiczeń medytacyjnych, służących rozwijaniu koncentracji świadomości i wzmacniania uwagi (wykład Rajabai Modi na seminarium w Nowym Jorku, w październiku 1983). Uwaga rozwiniętego Yogina działa jak system naprowadzania wibracji. Niczym promień lasera wystrzelony z platformy Nirvichary (świadomości bez myśli), oświecona uwaga koncentruje shakti Chaitanya (siłę wibracji) na zidentyfikowanej sferze negatywności. Jak napędzany mantrą pocisk, kieruje ona wibracje określonego Bóstwa lub Boskiego aspektu przeciwko temu punktowi negatywności, który odpowiada złej, antagonistycznej sile, będącej przeciwieństwem do tego aspektu.
+
+W obu wypadkach rezultaty mogą być dwojakie. Negatywność albo zostanie starta na proch, albo okaże się zbyt silna, żeby ulec zniszczeniu. Wówczas jednak chitta (uwaga) natychmiast włącza się do Sieci Telekomunikacyjnej Sahajlandu i przekazuje wszystkie stosowne informacje dotyczące przeszkody, do nieświadomości, która kieruje je do Najwyższego Dowództwa. (Zobacz "Nirmala Yoga", styczeń-luty 1983, str. 32). Najwyższe Dowództwo przejmuje dowodzenie. W Epoce Wodnika buce mają bardzo ciężko, ale nie mamy obowiązku ich żałować.
+
+Pole bitwy
+
+Są dwa typy pola bitwy:
+
+mikrokosmos pojedynczej istotymakrokosmos, pole kosmicznej walki pomiędzy devas i aniołami Shri Mataji z szatanem, kolektywnej zasadzie zła.
+
+Kiedy uwolnicie to pierwsze (mikrokosmos) od wrogiego wpływu, przyłączacie się do drugiego już jako precyzyjnie nastrojony instrument. Walka toczy się na różnych poziomach. Ale można chyba powiedzieć, że to wciąż jeszcze jest w dużej mierze partyzancka walka yoginów, bojowników o wolność przeciwko różnym władcom ciemności, którzy zniewolili tę planetę; przeciwko ich oddziałom złożonym z ludzi, ex-ludzi, mniej niż ludzi. To całkiem interesujące uświadomić sobie, jak w czasie walki na polu Kurów (Kurukshetra), Shri Krishna, który jest władcą dharmy uczył Pandawów pozbywać się reguł wojennej dharmy ich czasów. W dzisiejszej batalii również jest tak, że wyższy cel manifestacji zwycięstwa Ducha unieważnia wszystkie mniejsze dharmy. Dlatego właśnie w siłach zbrojnych Sahajlandu istnieje tak wielka różnorodność działań, manewrów, stylów i kamuflaży. Nikt nie jest w stanie przewidzieć, w jaki sposób one zadziałają!
+
+Taktyka i strategia
+
+Pierwsze pytanie: "Czy powinniśmy walczyć z negatywnością, czy lepiej jej unikać?"
+
+Shri Mataji odpowiedziała: "Jeśli możecie walczyć, róbcie to, jeśli nie, uciekajcie".
+
+Zatem elastyczna reakcja, która uwzględnia wszystkie sytuacje.
+
+Pierwsza zasada: skoncentrujecie wszystkie swoje siły na precyzyjnym wskazaniu tego punktu pola bitwy, gdzie linię wroga da się łatwo przełamanć.
+
+Jak? – Poprzez skierowanie uwagi na Shri Mataji.
+
+Dlaczego? – Wszystkie anioły, ganas, moce natychmiast ruszają nam na ratunek, dosiadając promieni emitowanych przez Jej Lotosowe Stopy.
+
+W końcu przecież, Bogini jest nazywana "Ranapandita", mistrzynią wiedzy o wojnach i bitwach. Zatem możemy teraz ograniczyć się do zdefiniowania podstawowych zasad działań wojennych sahaj. Sahaja wojownik musi tylko wiedzieć, że ma przywołać uwagę Boskiej Matki na swoje pole Kurów. "Uderzaj Arjuno, wszyscy ci nieprzyjaciele już zostali przeze Mnie uśmierceni", Shri Krishna jasno przedstawił swoim uczniom, że to On jest rzeczywistym Wykonawcą. Sahaja Yoga dowodzi, że ta prawda jest wieczna.
+
+Ze względu na strategię, tajnych informacji, dostępnych tylko dla wąskiego grona najbardziej zaufanych yoginów, nie można ujawnić. Jednak nawet ci yogini nie znają planu długoterminowego Najwyższego Dowództwa, który jest sporządzany poza czasem i przestrzenią, a więc poza mentalnymi kalkulacjami...użyteczne zabezpieczenie przed szpiegami Avidya Landu. Przez całe wieki próbowano zlokalizować Armagedon, miejsce ostatniej bitwy. Ale...kiedy ostatni poszukiwacz opuści Sodomę i Gomorę, kiedy armia rozbije obóz pod Sambalpur, kiedy wszyscy yogini na ziemi zintegrują się w jednej Sieci Telekomunikacyjnej Sahajlandu, kiedy wszystkie konie staną się białe jak śnieg...prawdopodobnie poznamy odpowiedź.
