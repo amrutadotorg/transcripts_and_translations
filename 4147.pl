83c25715244da54b505ff96af8b79b930125d4de
amrutadotorg <amruta.org@gmail.com>
2022-10-31 08:00:12 +0100

    new

---
+++ 4147.pl
@@ -0,0 +1,61 @@
+Pudźa Urodzinowa
+
+21 marca 1999, Delhi, Indie
+
+(Przekład z hindi)
+
+Po zobaczeniu tej miłości, moje serce jest wypełnione. I Ja myślę, że miłość jest tak wielką mocą, która przyciąga tak wielu ludzi i stają się szczęśliwymi. To taka zaskakująca rzecz. W tej Kalijudze [obecny wiek konfliktów], nikt nie widziałby tej wielkości miłości. Myślę, że po zobaczeniu tego, wszyscy powinniście nauczyć się powiększać swoją miłość. A to jest bardzo proste. I możecie to zrobić w ten sposób – medytując rano i wieczorem, wtedy złe myśli w twoim umyśle, które powodują zazdrość i złość i ranią cię małe rzeczy, wszystkie takie myśli się skończą. A po tym, to co pozostaje, jest Nirmal prema [czysta miłość do wszystkich istot] . Dzięki tej miłości możesz dać całemu światu nowe życie.
+
+(Przekład z angielskiego)
+
+Powiedziałam im w języku hindi, że miłość, możecie zobaczyć miłość jednej osoby przejawiającą się tutaj w tak wielu oczach. A gdy ujrzałam entuzjazm, również Moje serce napełniło się wielką miłością, większą miłością.
+
+Można więc zobaczyć, jak potężna jest ta miłość. Wszystkie rodzaje złych uczuć, negatywne myślenie, a także elementy autodestrukcyjne mogą być poprawione i kontrolowane bardzo prosto w Sahadźa Jodze, jeśli ze szczerym sercem medytujecie rano i wieczorem, dwa razy. W tym czasie nie powinniście spoglądać na zegarki, oczywiście, ale cieszyć się, cieszyć się medytacją. To da wam taką siłę, by kochać innych, przebaczać innym; zamiast czuć się źle z powodu małych, małych rzeczy, czujecie się zirytowani, bo uważacie, że wasz szacunek do samego siebie jest zagrożony, albo czasami stajemy się agresywni wobec pewnych ludzi, bardzo agresywni, bez ich winy.
+
+Najlepiej widzieć wibracje innej osoby. Jeśli wibracje są złe, walka nie ma sensu, spowoduje to większe zamieszanie. Osoba, która ma bardzo złe wibracje, nie możecie z nią walczyć, nie możecie kontrolować tej osoby. Albo osoba, która jest zbyt zarozumiała, nie powinniście próbować jej uspokajać lub próbować z nią negocjować, a czasem ludzie próbują rozpieszczać jej próżność. Robiąc to, psujecie ją bardziej. Więc najlepszą rzeczą jest okazanie swojej miłości w bardzo słodki i odmienny sposób.
+
+Dam wam przykład, który podawałam wiele razy, że poszłam zobaczyć Gagangarha Maharadźę [święty z Maharasztry; 1906 - 2008], który mieszkał na bardzo wysokiej górze [Garbhagiri hill koło Ratnagiri], i nie było jak wjechać samochodem ani nic, i musiałam iść pieszo .
+
+Więc wszyscy mówili: „Matko, po co Ty masz tam jechać mimo wszystko?”
+
+Odparłam: „Zobaczcie wibracje. Dochodzą dobre wibracje, w porządku? Dlatego jadę.
+
+Poza tym on mówił o Mnie wielu ludziom i on rzekł, że „Adi Śakti urodziła się w Bombaju, dlaczego przychodzicie do mnie?” Tak nam powiedzieli.
+
+Rzekłam więc: „Muszę zobaczyć tego człowieka” i tak oto pojechałam w to miejsce, żeby się z nim spotkać.
+
+Otóż mniema się, że on kontroluje deszcz. Potrafił kontrolować deszcz. Więc siedział on tam na kamieniu i kręcił głową z wielkim niepokojem, kiedy tam dotarłam. I padało tak mocno, że kiedy doszłam do niego, byłam kompletnie przemoczona. Weszłam więc do jaskini, w której mieszkał, a on wrócił z wielką złością na deszcz.
+
+Rzekł więc: „Matko, dlaczego nie pozwoliłaś mi opanować deszczu?”
+
+Odparłam: „Nie zrobiłam tego”.
+
+„Nie, Ty to zrobiłaś; bo zawsze mogę kontrolować deszcz, a Ty szłaś całą drogę z moim zaproszeniem, a ten deszcz nie powinien był się źle zachowywać”.
+
+Odparłam: „Nie, nie, on nie zachował się źle”. Więc co się stało, był w dość złym humorze, wiecie. Rzekłam: „Uspokój się, powiem ci, co się stało. Widzisz, kupiłeś dla Mnie sari a jesteś sannjasinem, a ja nie mogę wziąć sari od sannjasina, więc deszcz uczynił taką sposobność. Teraz wszystko mam przemoczone, więc będę musiała wziąć twoje sari.
+
+Widzicie, natychmiast poczuł do Mnie straszną miłość, zaczął płakać i upadł do Moich stóp. A potem rzekł: „Matko, teraz wiem, czym jest wielkość miłości, jak odciąga cię od przyziemnych rzeczy i suchego zachowania i zabiera cię do miejsca, w którym możesz cieszyć się deszczem miłości”.
+
+To jedna z historii, które wam opowiadam. Ale najważniejszą rzeczą w całym epizodzie jest to, że powinniście spróbować swojej miłości, swoich sztuczek czystej miłości, czystej miłości, i to działa. I musicie się uczyć, stopniowo nauczycie się próbować. Nie należy się poddawać. Niektórzy ludzie są bardzo trudni, zgadzam się – bardzo, bardzo trudni. Ale przynajmniej spróbujcie z ludźmi, którzy nie są tacy trudni. I będziecie zdumieni, że będziecie mieli dobre towarzystwo i mnóstwo przyjaciół i przyjaźni, tak jak my mamy teraz w Sahadźa Jodze.
+
+Gdy pierwszy raz przybyłam do Delhi, tak przeraziłam się tego miejsca, bo Sahadźa joginów było bardzo niewielu i przynieśli – nie wiem, dlaczego chcieli zrobić Moją pudźę, być może ludzie z Bombaju musieli im powiedzieć coś – i przynieśli kun-kum i wszystko w plastikowych, małych, buteleczkach – o, Ba! Doprawdy skurczyłam się, wiecie, z ich ignorancji. Powiedziałam: „Co teraz zrobić? Co się stanie?" Ale dzisiaj, widzicie, to samo Delhi, które stało się tak wielkie, i tak piękne, i tak entuzjastyczne. Widziałam ich z ich flagami i nie wiedziałam, że będzie tu taki korowód flag, chociaż mieliśmy raz czy dwa, jak sądzę, w Cabelli. Ale jak oni cieszyli się nawzajem! To naprawdę niezwykłe.
+
+Jeśli potrafisz cieszyć się miłością, nie będziesz cieszyć się niczym innym poza miłością do innych ludzi, a nie do siebie. Wręcz przeciwnie, zobaczysz, że twoje ciało, twój umysł, twoje myśli, wszystko jest skierowane na tworzenie miłości do innych, a nie do siebie. Zmienia się całe nastawienie. Tak jak w ciemności nie możecie zobaczyć niczego, ale nawet jeśli jest tam małe światełko, ono rozchodzi się wszędzie. Nikt nie mówi światłu, że „Musisz rozproszyć światło wszędzie dookoła”, ale ono to robi. W taki sam sposób, wy wszyscy jesteście oświeconymi ludźmi, wszyscy jesteście oświeceni miłością; i światło tej miłości rozprzestrzenia się automatycznie, spontanicznie, sahadźa.
+
+Byłam również bardzo oczarowana sposobem, w jaki nasza Juwa Śakti tańczyła i cieszyła się. Myślę, że to bardzo wielkie błogosławieństwo, bo w dzisiejszych czasach nasze małe dzieci, nasi młodzi ludzie błądzą. Oni nie są tak źli, jak w innych krajach, ale mamy do czynienia z dość dużym trendem, który psuje ich i czyni z nich coś, co można by nazwać bardzo zachodnią, zepsutą osobowością. Ale teraz widziałam, jak wielu z nich tańczy razem, zapominając o swojej kaście, wyznaniu, wszystkim i ciesząc się swoim towarzystwem.
+
+To jest coś, co musi się wydarzyć w tym kraju i w każdym kraju, że wszyscy powinniśmy czuć jedność i cieszyć się tą jednością ze wszystkimi. Jeśli się nie cieszymy, wtedy jesteśmy jak kropla poza oceanem, która możne wyschnąć w każdej chwili. Ale jeśli jesteście nieodłączną częścią tego oceanu, to dzieje się tak, że każdy ruch oceanu niesiecie ze sobą i jesteście nieodłączną częścią tego oceanu całkowicie, bo nie macie odrębnej tożsamości.
+
+Wszystkie te rzeczy, które tworzą odrębną tożsamość w naszym społeczeństwie lub w naszej kulturze lub w naszym zachowaniu, w życiu osobistym, powinny być całkowicie przykrócone. A to rozwiąże wiele problemów, od życia rodzinnego, przez życie narodowe, po życie międzynarodowe. Powinniśmy więc okiełznać wszelkie takie poczucia separacji, że powinniśmy mieć osobny dom, odrębne państwo, osobny naród.
+
+Stopniowo wszystkie narody staną się jednym, Ja jestem tego pewna; bo nadchodzą wspaniałe czasy i tak wielu ludzi u steru spraw przejdzie do Sahadźa Jogi. Kiedy oni podejmą Sahadźa Jogę, wtedy sprawy będą wyglądać zupełnie inaczej. Dziś nie ma ich z nimi, Sahadźa Joga nie jest z nimi. Nie mają wiedzy o naszym wielkim ruchu miłości, więc oni chcą oddzielić się od wszystkiego. Oni nie wiedzą, że jutro zostaną wypaleni przez żar wokół, a może przez deszcz zostaną zmyci, a być może, że Matka Ziemia może ich wchłonąć.
+
+Więc wszyscy powinniśmy być razem, bez względu na różnice. W różnych rodzinach się rodzimy – ostatecznie, wszyscy nie mogą urodzić się w jednej rodzinie. Ale teraz należycie do rodziny Sahadźa Jogi, a ta rodzina Sahadźa Jogi jest jedna. Ona nie wierzy w posiadanie odrębnej egzystencji odrębnej specjalności. Wszyscy jesteśmy ze sobą jednym i nie obchodzi nas, jakie różnice są na zewnątrz.
+
+Byłam naprawdę bardzo poruszona, ponieważ teraz nadchodzi nowy rok z nowymi wyzwaniami dla nas wszystkich, które musimy zaakceptować, że Kalijuga się skończyła i my musimy ustanowić Satjajugę [wiek prawdy]; dla której wy wszyscy, z każdego kraju powinniście pomyśleć, jak możecie to zrobić w swoim i w innym kraju. Jakie są problemy w innym kraju? Skierujcie swoją uwagę na zewnątrz, a nie do wewnątrz w ten sposób, że miałbyś rzec, że potrzebuję tego, potrzebuję tamtego, potrzebuję jeszcze coś. Powinniśmy wiedzieć, czego potrzebują inni ludzie: jakie są ich potrzeby, czego ludzie w naszym społeczeństwie, w naszym narodzie, w naszym świecie wymagają? Dobrze, jak to zapiszecie: czego oni wymagają? Tak jest lepiej, to zadziała; być może, to może zostać poprawione. Bardzo ważne jest, aby Sahadźa jogini usiedli i zapisali, czego potrzebuje świat i co należy zrobić.
+
+To będzie dobry pomysł dla was wszystkich, aby naprawdę aspirować do tego rodzaju jednorodnego społeczeństwa, które mamy tutaj dzisiaj. A my będziemy pewnego dnia, będziecie zaskoczeni, że będziemy tymi, którzy pokierują i poprowadzą resztę ludzi z naszą miłością, uwagą i troską.
+
+Więc to jest bardzo ważny czas i w tym czasie wszyscy powinniśmy myśleć w ten sposób.
+
+Dziękuję bardzo. Niech Bóg was błogosławi.
